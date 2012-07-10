cc8cf653773bd80ed9049d8448528284e2b6089f
Mihai Parparita <mihaip@chromium.org>
2012-07-10 10:23:02 -0700

    Update singleton sample to use new focusing API.

--- singleton/main.js
+++ singleton/main.js
@@ -3,7 +3,7 @@ var singletonWindow;
 chrome.experimental.app.onLaunched.addListener(function() {
   if (singletonWindow && !singletonWindow.closed) {
     console.log('Focusing singleton window');
-    singletonWindow.focus();
+    singletonWindow.chrome.appWindow.focus();
   } else {
     console.log('Creating singleton window');
     chrome.appWindow.create('singleton.html', {}, function(w) {
