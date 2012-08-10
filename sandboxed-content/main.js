d60580921f554c61ba075b75714a71e53ebd7100
Paul Lewis <paullewis@google.com>
2012-08-10 12:55:51 +0100

    Updated icon, tweaked manifest and main.js

--- sandboxed-content/main.js
+++ sandboxed-content/main.js
@@ -1,6 +1,10 @@
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/trunk/apps/experimental.app.html
+ * @see http://developer.chrome.com/trunk/apps/app.window.html
+ */
 chrome.experimental.app.onLaunched.addListener(function() {
-  chrome.app.window.create('main.html', {
-    'width': 400,
-    'height': 400
-  });
+  chrome.app.window.create('main.html',
+    {width: 400, height: 350});
 });
