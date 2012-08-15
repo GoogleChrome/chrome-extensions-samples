eec6690e33efc32ed482157e9f976698b1bd9ecf
Sam Thorogood <sam.thorogood@gmail.com>
2012-08-15 14:32:20 +1000

    Rename experimental.app.onLaunched to app.runtime.onLaunched

--- mdns-browser/background.js
+++ mdns-browser/background.js
@@ -1,4 +1,4 @@
-chrome.experimental.app.onLaunched.addListener(function() { 
+chrome.app.runtime.onLaunched.addListener(function() {
   var w = chrome.appWindow || chrome.app.window;
   w.create('main.html', {
     type: 'shell',
