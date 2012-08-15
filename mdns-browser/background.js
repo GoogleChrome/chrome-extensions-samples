ff0522e833ff3e10dc32ad366376d47fb953bbf6
Sam Thorogood <sam.thorogood@gmail.com>
2012-08-15 14:20:05 +1000

    Added mDNS browser to samples (from github.com/samthor/mdns-browser)

---
+++ mdns-browser/background.js
@@ -0,0 +1,9 @@
+chrome.experimental.app.onLaunched.addListener(function() { 
+  var w = chrome.appWindow || chrome.app.window;
+  w.create('main.html', {
+    type: 'shell',
+    width: 440,
+    minWidth: 440,
+    minHeight: 200,
+  });
+});
