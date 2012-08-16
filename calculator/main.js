c6c0a75a3f00e6a299cc1c6e374e03ee19f4c3c4
Tamara Fleisher <tamara.fleisher@gmail.com>
2012-08-16 11:09:07 -0700

    chrome.experimental -> chrome.app.runtime

--- calculator/main.js
+++ calculator/main.js
@@ -1,4 +1,16 @@
-chrome.experimental.app.onLaunched.addListener(function() {
+/**
+ * Copyright (c) 2012 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ **/
+
+
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/trunk/apps/app.window.html
+ */
+chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('calculator.html', {
     width: 244,
     height: 380,
