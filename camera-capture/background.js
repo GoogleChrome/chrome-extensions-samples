e828d9bee32f1d5a10b6b2fc2673e433ed22502a
Paul Lewis <paullewis@google.com>
2012-08-17 16:30:48 +0100

    Updated camera-capture

--- camera-capture/background.js
+++ camera-capture/background.js
@@ -1,3 +1,9 @@
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/trunk/apps/app.runtime.html
+ * @see http://developer.chrome.com/trunk/apps/app.window.html
+ */
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
     width: 700,
