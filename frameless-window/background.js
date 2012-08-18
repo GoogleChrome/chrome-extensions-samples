e0883e477291f53a0f850ff9b5b36cc2bf08d061
Renato Mangini <mangini@html5rocks.com>
2012-08-17 22:37:08 -0300

    fixed code on frameless-window that was breaking running in dojo

--- frameless-window/background.js
+++ frameless-window/background.js
@@ -1,14 +1,11 @@
-chrome.experimental.app.onLaunched.addListener(function() {
-  chrome.app.window.create(
-    "frameless_window.html",
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create("frameless_window.html",
     {  width: 360,
        height: 300,
        minWidth: 220,
        minHeight: 220,
-       frame: 'none'
-    },
-    function(win) {
-      console.log(win);
+       left: 600
     }
   );
-})
+});
+
