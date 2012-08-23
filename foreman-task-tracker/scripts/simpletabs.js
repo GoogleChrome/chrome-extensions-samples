e6ffd428cf1e7bbe95cf85ffab1087ffbe76e85f
Trent Apted <tapted@google.com>
2012-08-23 16:02:45 +1000

    Initial commit of Foreman task tracker app with static data

---
+++ foreman-task-tracker/scripts/simpletabs.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	//Default Action
+	$(".tab_content").hide(); //Hide all content
+	$("ul.tabs li:first").addClass("active").show(); //Activate first tab
+	$(".tab_content:first").show(); //Show first tab content
+
+	//On Click Event
+	$("ul.tabs li").click(function() {
+		$("ul.tabs li").removeClass("active"); //Remove any "active" class
+		$(this).addClass("active"); //Add "active" class to selected tab
+		$(".tab_content").hide(); //Hide all tab content
+		var activeTab = $(this).find("a").attr("href"); //Find the rel attribute value to identify the active tab + content
+		$(activeTab).fadeIn(); //Fade in the active content
+		return false;
+	});
+
+});
