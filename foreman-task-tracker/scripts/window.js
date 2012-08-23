05f65b1a4298a82478467d42e2cd0b739488dbf3
Trent Apted <tapted@google.com>
2012-08-23 17:52:10 +1000

    tabbing tweaks

--- foreman-task-tracker/scripts/window.js
+++ foreman-task-tracker/scripts/window.js
@@ -1,3 +1,46 @@
+var activeTabAnchor;
+var activeTab;
+
+$(document).ready(function() {
+  //Default Action
+  $(".tab_content").hide(); //Hide all content
+  $("ul.tabs li:first").addClass("active").show(); //Activate first tab
+  $(".tab_content:first").show(); //Show first tab content
+
+  //On Click Event
+  $("ul.tabs li").click(function() {
+    if (activeTabAnchor) {
+      //restore this first, or we can not compare hrefs
+      activeTabAnchor.attr("href", activeTab);
+    }
+
+    //find where the tab link points to
+    var tabAnchor = $(this).find("a");
+    var tab = tabAnchor.attr("href");
+
+    if (tab == activeTab) {
+      //same tab clicked -- remove href again and do nothing further
+      activeTabAnchor.removeAttr("href");
+      return false;
+    }
+    if (activeTabAnchor) {
+      activeTabAnchor.attr("contentEditable", 'false');
+    }
+    activeTabAnchor = tabAnchor;
+    activeTab = tab;
+    if (tab != "#snapshots") {
+      activeTabAnchor.attr("contentEditable", 'true');
+    }
+
+    $("ul.tabs li").removeClass("active"); //Remove any "active" class
+    $(this).addClass("active"); //Add "active" class to selected tab
+    $(".tab_content").hide(); //Hide all tab content
+    activeTabAnchor.removeAttr("href");
+    $(activeTab).fadeIn(); //Fade in the active content
+    return false;
+  });
+});
+
 onload = function() {
   function update() {
 
@@ -15,3 +58,4 @@ onload = function() {
     };
   }
 }
+
