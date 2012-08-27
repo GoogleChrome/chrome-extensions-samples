d8e6dfeda9ff1468b4b9d646a4801cb924d9308f
Eric Bidelman <ebidel@gmail.com>
2012-08-27 15:35:18 -0700

    Updating readmes and authorship

---
+++ filesystem-access/README.md
@@ -0,0 +1,7 @@
+# Filesystem access
+
+Shows basic usage of accessing (read/write) files using the `chrome.fileSystem` API.
+
+## APIs
+
+* [chrome.fileSystem](http://developer.chrome.com/trunk/apps/fileSystem.html)
--- filesystem-access/js/app.js
+++ filesystem-access/js/app.js
@@ -1,3 +1,21 @@
+/*
+Copyright 2012 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Author: Eric Bidelman (ericbidelman@chromium.org)
+*/
+
 // document.addEventListener('load', function() {
 //  if(!!window.webkitIntent) {
 //    var action = window.webkitIntent.action;
@@ -65,10 +83,10 @@ var textarea = document.querySelector('textarea');
 
 
 chooseFileButton.addEventListener('click', function(e) {
-	chrome.fileSystem.chooseFile({type: 'openFile'}, function(readOnlyEntry) {
-		choosenFileEntry = readOnlyEntry;
+  chrome.fileSystem.chooseFile({type: 'openFile'}, function(readOnlyEntry) {
+    choosenFileEntry = readOnlyEntry;
 
-    // Can't restrict chooseFile to a mimetype. See https://crbug.com.133066.
+    // Can't restrict chooseFile to a mimetype. See https://crbug.com/133066.
     // Remove this lookup when that is supported.
     choosenFileEntry.file(function(file) {
       if (file.type.match(/text\/.*/)) {
@@ -83,16 +101,16 @@ chooseFileButton.addEventListener('click', function(e) {
         output.textContent = 'Not a text file';
       }
     });
-	});
+  });
 });
 
 // writeFileButton.addEventListener('click', function(e) {
 //   if (choosenFileEntry) {
-//   	chrome.fileSystem.getWritableFileEntry(choosenFileEntry, function(writableEntry) {
+//    chrome.fileSystem.getWritableFileEntry(choosenFileEntry, function(writableEntry) {
 //       writeFileEntry(writableEntry, null, function(e) {
 //         output.textContent = 'Write complete :)';
 //       });
-//   	});
+//    });
 //   }
 // });
 
