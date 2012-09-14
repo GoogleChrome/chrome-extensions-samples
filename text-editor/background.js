b17d0538a1ffc7a38b293e996c9efa05d4ddd5f2
Renato Mangini <mangini@gmail.com>
2012-09-14 14:41:49 -0700

    - changed AppWindow.dom to AppWindow.contentWindow; - fixed deprecated and wrong usage of frame param on window.create

--- text-editor/background.js
+++ text-editor/background.js
@@ -1,5 +1,5 @@
 chrome.app.runtime.onLaunched.addListener(function (launchData) {
   chrome.app.window.create('editor.html', function(win) {
-    win.dom.launchData = launchData;
+    win.contentWindow.launchData = launchData;
   });
 });
