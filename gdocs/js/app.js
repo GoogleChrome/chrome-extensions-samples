1dae25076a86b1d75970832b5c15a29dc1263f00
Eric Bidelman <ebidel@gmail.com>
2012-10-08 11:04:41 -0700

    Making app more angular-ly

--- gdocs/js/app.js
+++ gdocs/js/app.js
@@ -59,49 +59,19 @@ function DocsController($scope, $http) {
   $scope.fetchDocs = function() {
     $scope.docs = []; // Clear out old results.
 
-    var informAngular = function(doc, i, totalEntries) {
+    var onResponse = function(doc, i, totalEntries, opt_inform) {
+      var inform = opt_inform || false;
+
       $scope.docs.push(doc);
 
       // Only want to sort and call $apply() when we have all entries.
       if (totalEntries - 1 == i) {
         $scope.docs.sort(Util.sortByDate);
-        $scope.$apply(); // Inform angular that we made changes.
+        if (inform) {
+          $scope.$apply(); // Inform angular that we made changes.
+        }
       }
     };
-
-    // Response handler that doesn't cache file icons.
-    // var successCallback = function(resp, status, headers, config) {
-    //   var docs = [];
-
-    //   var totalEntries = resp.feed.entry.length;
-
-    //   resp.feed.entry.forEach(function(entry, i) {
-    //     var doc = {
-    //       title: entry.title.$t,
-    //       updatedDate: Util.formatDate(entry.updated.$t),
-    //       updatedDateFull: entry.updated.$t,
-    //       icon: gdocs.getLink(entry.link,
-    //                           'http://schemas.google.com/docs/2007#icon').href,
-    //       alternateLink: gdocs.getLink(entry.link, 'alternate').href,
-    //       size: entry.docs$size ? '( ' + entry.docs$size.$t + ' bytes)' : null
-    //     };
-
-    //     var xhr = new XMLHttpRequest();
-    //     xhr.open('GET', doc.icon, true);
-    //     xhr.responseType = 'blob';
-    //     xhr.onerror = onError;
-    //     xhr.onload = function(e) {
-    //       console.log('Fetched icon via XHR');
-
-    //       doc.icon = window.URL.createObjectURL(this.response);
-
-    //       informAngular(doc, i, totalEntries);
-    //     };
-
-    //     xhr.send();
-
-    //   });
-    // };
 
 
     // Response handler that caches file icons int he filesystem API.
@@ -124,31 +94,29 @@ function DocsController($scope, $http) {
         // 'http://gstatic.google.com/doc_icon_128.png' -> 'doc_icon_128.png'
         doc.iconFilename = doc.icon.substring(doc.icon.lastIndexOf('/') + 1);
 
+        // If file exists, it we'll get back a FileEntry for the filesystem URL.
+        // Otherwise, the error callback will fire and we need to XHR it in and
+        // write it to the FS.
         var fsURL = fs.root.toURL() + FOLDERNAME + '/' + doc.iconFilename;
         window.webkitResolveLocalFileSystemURL(fsURL, function(entry) {
+          console.log('Fetched icon from the FS cache');
+
           doc.icon = entry.toURL(); // should be === to fsURL, but whatevs.
 
-          informAngular(doc, i, totalEntries);
+          onResponse(doc, i, totalEntries, true);
         }, function(e) {
-          // Error: file doesn't exist yet. XHR it in and write it to the FS.
-          var xhr = new XMLHttpRequest();
-          xhr.open('GET', doc.icon, true);
-          xhr.responseType = 'blob';
-          xhr.onerror = onError;
-          xhr.onload = function(e) {
+
+          $http.get(doc.icon, {responseType: 'blob'}).success(function(blob) {
             console.log('Fetched icon via XHR');
 
-            var blob = e.target.response;
             blob.name = doc.iconFilename; // Add icon filename to blob.
 
             writeFile(blob); // Write is async, but that's ok.
 
             doc.icon = window.URL.createObjectURL(blob);
 
-            informAngular(doc, i, totalEntries);
-          };
-
-          xhr.send();
+            onResponse(doc, i, totalEntries);
+          });
 
         });
       });
