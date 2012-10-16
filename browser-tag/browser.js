f818cff2d102a483ebb14dc8925086134d83c6ca
Mihai Parparita <mihaip@chromium.org>
2012-10-16 15:32:16 -0700

    Update <browser> sample with new events and methods.
    http://crrev.com/161734 exposed additional browser plugin methods and events to
    the <browser> shim. This uses them to make the sample into a more realistic
    browser:
    - Add back/forward buttons
    - Update the URL bar when navigating
    - Handle crashes (complete with sad tab-like page)

--- browser-tag/browser.js
+++ browser-tag/browser.js
@@ -1,27 +1,45 @@
 window.onresize = doLayout;
 
 onload = function() {
+  var browser = document.querySelector('browser');
   doLayout();
 
-  document.querySelector('#new-browser').onclick = function() {
-    chrome.app.window.create('browser.html', {
-      'width': 1024,
-      'height': 768
-    });
+  document.querySelector('#back').onclick = function() {
+    browser.back();
+  };
+
+  document.querySelector('#forward').onclick = function() {
+    browser.forward();
   };
 
   document.querySelector('#home').onclick = function() {
-    document.querySelector('browser').src =
-      document.querySelector('#location').value = 'http://www.google.com/';
+    navigateTo('http://www.google.com/');
+  };
+
+  document.querySelector('#reload').onclick = function() {
+    browser.reload();
+  };
+
+  document.querySelector('#terminate').onclick = function() {
+    browser.terminate();
   };
 
   document.querySelector('#location-form').onsubmit = function(e) {
     e.preventDefault();
-
-    document.querySelector('browser').src =
-        document.querySelector('#location').value;
+    navigateTo(document.querySelector('#location').value);
   };
+
+  browser.addEventListener('crash', handleCrash);
+  browser.addEventListener('navigation', handleNavigation);
+  browser.addEventListener('loadStart', handleLoadStart);
+  browser.addEventListener('loadAbort', handleLoadAbort);
+  browser.addEventListener('loadRedirect', handleLoadRedirect);
 };
+
+function navigateTo(url) {
+  document.body.classList.remove('crashed');
+  document.querySelector('browser').src = url;
+}
 
 function doLayout() {
   var browser = document.querySelector('browser');
@@ -32,5 +50,46 @@ function doLayout() {
   browser.width = windowWidth;
   browser.height = windowHeight - controlsHeight;
 
-  console.log(windowWidth, windowHeight);
+  var sadBrowser = document.querySelector('#sad-browser');
+  sadBrowser.style.width = browser.width + 'px';
+  sadBrowser.style.height = browser.height * 2/3 + 'px';
+  sadBrowser.style.paddingTop = browser.height/3 + 'px';
+}
+
+function handleCrash(event) {
+  document.body.classList.add('crashed');
+}
+
+function handleNavigation(event) {
+  document.body.classList.remove('crashed');
+  if (!event.isTopLevel) {
+    return;
+  }
+
+  document.querySelector('#location').value = event.url;
+}
+
+function handleLoadStart(event) {
+  document.body.classList.remove('crashed');
+  if (!event.isTopLevel) {
+    return;
+  }
+
+  document.querySelector('#location').value = event.url;
+}
+
+function handleLoadAbort(event) {
+  console.log('oadAbort');
+  console.log('  url: ' + event.url);
+  console.log('  isTopLevel: ' + event.isTopLevel);
+  console.log('  type: ' + event.type);
+}
+
+function handleLoadRedirect(event) {
+  document.body.classList.remove('crashed');
+  if (!event.isTopLevel) {
+    return;
+  }
+
+  document.querySelector('#location').value = event.newUrl;
 }
