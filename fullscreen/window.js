642818d39c4eb297b25c91e13fbaf39bb1652499
Vincent Scheib <scheib@gmail.com>
2012-12-06 10:28:18 -0800

    Fullscreen is not possible without a user gesture, comment link to bug.

--- fullscreen/window.js
+++ fullscreen/window.js
@@ -19,5 +19,7 @@ document.getElementById('exit').addEventListener('click', function(e) {
 });
 
 // Attempt fullscreen on window creation.
+// It will fail, but hopefully some day it won't:
+// http://code.google.com/p/chromium/issues/detail?id=164624
 document.body.webkitRequestFullscreen();
 
