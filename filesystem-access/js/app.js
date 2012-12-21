b586b05b9dedb2abed9e5266ef0dd471890d9c9d
Renato Mangini <mangini@gmail.com>
2012-12-21 15:57:58 -0200

    fixed issue #56: file truncate required before write

--- filesystem-access/js/app.js
+++ filesystem-access/js/app.js
@@ -70,13 +70,41 @@ function writeFileEntry(writableEntry, opt_blob, callback) {
     // If we have data, write it to the file. Otherwise, just use the file we
     // loaded.
     if (opt_blob) {
-      writer.write(opt_blob);
+      writer.truncate(opt_blob.size);
+      waitForIO(writer, function() {
+        writer.seek(0);
+        writer.write(opt_blob);
+      });
     } else {
       chosenFileEntry.file(function(file) {
-        writer.write(file);
+        writer.truncate(file.fileSize);
+        waitForIO(writer, function() {
+          writer.seek(0);
+          writer.write(file);
+        });
       });
     }
   }, errorHandler);
+}
+
+function waitForIO(writer, callback) {
+  // set a watchdog to avoid eventual locking:
+  var start = Date.now();
+  // wait for a few seconds
+  var reentrant = function() {
+    if (writer.readyState===writer.WRITING && Date.now()-start<4000) {
+      setTimeout(reentrant, 100);
+      return;
+    }
+    if (writer.readyState===writer.WRITING) {
+      console.error("Write operation taking too long, aborting!"+
+        " (current writer readyState is "+writer.readyState+")");
+      writer.abort();
+    } else {
+      callback();
+    }
+  };
+  setTimeout(reentrant, 100);
 }
 
 var chosenFileEntry = null;
