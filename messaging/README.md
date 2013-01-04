04e2bc918b56fd4c28e422b4bb76b0df4a522d40
Renato Mangini <mangini@gmail.com>
2013-01-04 20:43:53 -0200

    Added messaging sample

---
+++ messaging/README.md
@@ -0,0 +1,9 @@
+# Messaging
+
+This sample shows how to communicate between two apps or one app and one extension. Add app1, app2 and extension to Chrome and play with sending messages between them.
+
+## Resources
+
+* [Messaging](https://developer.chrome.com/trunk/apps/runtime.html#method-sendMessage)
+* [Runtime](http://developer.chrome.com/trunk/apps/app.runtime.html)
+* [Window](http://developer.chrome.com/trunk/apps/app.window.html)
---
+++ messaging/app2/index.js
@@ -0,0 +1,44 @@
+(function(context){
+
+  document.getElementById("appid").value=chrome.runtime.id;  
+  var logField = document.getElementById("log");
+  var sendText=document.getElementById("sendText");
+  var sendText=document.getElementById("sendText");
+  var sendId=document.getElementById("sendId");
+  var send=document.getElementById("send");
+
+  send.addEventListener('click', function() {
+    appendLog("sending to "+sendId.value);
+    chrome.runtime.sendMessage(
+      sendId.value, 
+      {myCustomMessage: sendText.value}, 
+      function(response) { 
+        appendLog("response: "+JSON.stringify(response));
+      })
+  });
+
+
+  blacklistedIds = ["none"];
+
+  chrome.runtime.onMessageExternal.addListener(
+    function(request, sender, sendResponse) {
+      if (sender.id in blacklistedIds) {
+        sendResponse({"result":"sorry, could not process your message"});
+        return;  // don't allow this extension access
+      } else if (request.myCustomMessage) {
+        appendLog("from "+sender.id+": "+request.myCustomMessage);
+        sendResponse({"result":"Ok, got your message"});
+      } else {
+        sendResponse({"result":"Ops, I don't understand this message"});
+      }
+    });
+
+
+
+  var appendLog = function(message) {
+    logField.innerText+="\n"+message;
+  }
+
+  context.appendLog = appendLog;
+
+})(window)
