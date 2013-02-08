49c23fd29318bd5031e18e2965a899da2d9b5c46
Justin DeWitt <dewittj@chromium.org>
2013-02-08 10:29:08 -0800

    Style cleanup.

--- push-guestbook/guestbook-srv/guestbook.py
+++ push-guestbook/guestbook-srv/guestbook.py
@@ -1,45 +1,58 @@
-#!/usr/bin/env python
-import os
-import sys
+"""Demo application server that can push messages via the Cloud Messaging API.
 
-sys.path.append(os.path.join(os.path.dirname(__file__), 'third_party'))
+Normal execution should be along the following lines:
+  Start the server:
+    $ dev_appserver.py .
+  Enable push messaging by navigating to:
+    http://localhost:8080/startpush
+  Sign guestbook, and any guestbook-app clients should get the most recent
+  message.
+"""
 
-import httplib2
 import logging
+import os
 import random
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), 'third_party'))
+import httplib2
+from oauth2client.appengine import CredentialsModel
+from oauth2client.appengine import StorageByKeyName
+from oauth2client.client import AccessTokenRefreshError
+from oauth2client.client import flow_from_clientsecrets
 import simplejson
 import webapp2
 
-from google.appengine.ext import db
 from google.appengine.api import users
+from google.appengine.ext import db
 from google.appengine.ext.webapp import template
-
-from oauth2client.appengine import StorageByKeyName, CredentialsModel
-from oauth2client.client import flow_from_clientsecrets, AccessTokenRefreshError
 
 CLIENTSECRETS_LOCATION = 'client_secrets.json'
 REDIRECT_URI = 'http://localhost:8080/oauth2callback'
-SCOPES = [ 'https://www.googleapis.com/auth/gcm_for_chrome' ]
+SCOPES = ['https://www.googleapis.com/auth/gcm_for_chrome']
 
 logger = logging.getLogger(__name__)
+
 
 class AdminCredentialsModel(CredentialsModel):
   pass
 
-def send_messages(post_data):
-  storage = StorageByKeyName(AdminCredentialsModel, 'theadminaccount', 'credentials')
+
+def SendMessages(post_data):
+  storage = StorageByKeyName(AdminCredentialsModel,
+                             'theadminaccount',
+                             'credentials')
   credentials = storage.get()
   if credentials:
     try:
       api_http = credentials.authorize(httplib2.Http())
       for data in post_data:
-        resp, content = api_http.request(
+        api_http.request(
             'https://www.googleapis.com/gcm_for_chrome/v1/messages',
             'POST',
             body=simplejson.dumps(data),
             headers={'Content-Type': 'application/json'})
     except AccessTokenRefreshError:
-      logger.warning("Unable to refresh the Push Messaging access token!")
+      logger.warning('Unable to refresh the Push Messaging access token!')
 
 
 class Greeting(db.Model):
@@ -47,10 +60,12 @@ class Greeting(db.Model):
   content = db.StringProperty(multiline=True)
   date = db.DateTimeProperty(auto_now_add=True)
 
+
 class Follower(db.Model):
   channelId = db.StringProperty()
   verified = db.BooleanProperty()
   verifier = db.IntegerProperty()
+
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
@@ -64,14 +79,15 @@ class MainPage(webapp2.RequestHandler):
       url_linktext = 'Login'
 
     template_values = {
-      'greetings': greetings,
-      'url': url,
-      'url_linktext': url_linktext
+        'greetings': greetings,
+        'url': url,
+        'url_linktext': url_linktext
     }
 
     template_path = os.path.join(os.path.dirname(__file__), 'index.html')
     self.response.out.write(template.render(template_path, template_values))
     return
+
 
 class Guestbook(webapp2.RequestHandler):
   def post(self):
@@ -84,25 +100,25 @@ class Guestbook(webapp2.RequestHandler):
     greeting.put()
 
     followers = Follower.all().filter('verified =', True)
-    #followers = Follower.all()
     channel_ids = [f.channelId for f in followers if f.channelId]
     post_data = [{
         'channelId': channelId,
         'subchannelId': 0,
-        'payload':greeting.content
-      } for channelId in channel_ids]
-    send_messages(post_data)
+        'payload': greeting.content
+    } for channelId in channel_ids]
+    SendMessages(post_data)
 
     self.redirect('/')
 
+
 class Monitor(webapp2.RequestHandler):
-  def send_verification_message(self, follower):
+  def SendVerificationMessage(self, follower):
     post_data = {
-      "channelId": follower.channelId,
-      "subchannelId": 1,
-      "payload": follower.verifier
+        'channelId': follower.channelId,
+        'subchannelId': 1,
+        'payload': follower.verifier
     }
-    send_messages([post_data])
+    SendMessages([post_data])
 
   def post(self):
     args = simplejson.loads(self.request.body)
@@ -114,17 +130,18 @@ class Monitor(webapp2.RequestHandler):
                                         verifier=random.randrange(100000),
                                         verified=False)
       if verifier is not None and int(verifier) == follower.verifier:
-        follower.verified=True
+        follower.verified = True
         follower.put()
-      elif follower.verified == False:
-        self.send_verification_message(follower)
-      response = { 'status': 'ok' }
+      elif not follower.verified:
+        self.SendVerificationMessage(follower)
+      response = {'status': 'ok'}
       last_message = Greeting.all().order('-date').fetch(1)
-      if len(last_message) > 0:
+      if last_message:
         response['lastMessage'] = last_message[0].content
       self.response.out.write(simplejson.dumps(response))
       return
     self.response.out.write(simplejson.dumps(dict(status='err')))
+
 
 class StartPushOAuth(webapp2.RequestHandler):
   def get(self):
@@ -139,6 +156,7 @@ class StartPushOAuth(webapp2.RequestHandler):
     auth_uri = flow.step1_get_authorize_url()
     self.redirect(str(auth_uri))
 
+
 class OAuth2Redirect(webapp2.RequestHandler):
   def get(self):
     flow = flow_from_clientsecrets(CLIENTSECRETS_LOCATION,
@@ -146,14 +164,17 @@ class OAuth2Redirect(webapp2.RequestHandler):
                                    redirect_uri=REDIRECT_URI)
     flow.params['access_type'] = 'offline'
     credentials = flow.step2_exchange(self.request.get('code'))
-    storage = StorageByKeyName(AdminCredentialsModel, 'theadminaccount', 'credentials')
+    storage = StorageByKeyName(AdminCredentialsModel,
+                               'theadminaccount',
+                               'credentials')
     storage.put(credentials)
     self.redirect('/')
 
+
 app = webapp2.WSGIApplication([
-  ('/', MainPage),
-  ('/sign', Guestbook),
-  ('/monitor', Monitor),
-  ('/startpush', StartPushOAuth),
-  ('/oauth2callback', OAuth2Redirect)
+    ('/', MainPage),
+    ('/sign', Guestbook),
+    ('/monitor', Monitor),
+    ('/startpush', StartPushOAuth),
+    ('/oauth2callback', OAuth2Redirect)
 ], debug=True)
