110f46b526f162ac7220bbf1b360e2e3c7f24b72
Vincent Scheib <scheib@gmail.com>
2013-04-03 17:23:20 -0700

    New window creation options of Minimized, Maximized.
    Upcoming feature work http://crbug.com/163626 will enable window.create
    to create windows already fullscreen, maximized, or minimized.

--- window-state/background.js
+++ window-state/background.js
@@ -1,3 +1,8 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('window.html');
+  chrome.app.window.create('window.html', {
+    bounds: {
+      width: 500,
+      height: 500
+    }
+  });
 });
