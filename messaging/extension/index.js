e488da6619db11c6b250152d69f439b7a91d5094
Renato Mangini <mangini@chromium.org>
2013-04-10 17:44:55 -0300

    moved the onMessage part of the extension in messaging sample to an event page, so it can receive message even when the popup is not showing.

--- messaging/extension/index.js
+++ messaging/extension/index.js
@@ -17,24 +17,6 @@
       })
   });
 
-
-  blacklistedIds = ["none"];
-
-  chrome.runtime.onMessageExternal.addListener(
-    function(request, sender, sendResponse) {
-      if (sender.id in blacklistedIds) {
-        sendResponse({"result":"sorry, could not process your message"});
-        return;  // don't allow this extension access
-      } else if (request.myCustomMessage) {
-        appendLog("from "+sender.id+": "+request.myCustomMessage);
-        sendResponse({"result":"Ok, got your message"});
-      } else {
-        sendResponse({"result":"Ops, I don't understand this message"});
-      }
-    });
-
-
-
   var appendLog = function(message) {
     logField.innerText+="\n"+message;
   }
