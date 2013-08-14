b72e7930b06621d6b112c75cfc2e336df3654ad6
Vincent Scheib <scheib@gmail.com>
2013-08-14 13:01:59 -0400

    fit image to canvas

--- image-edit/app.js
+++ image-edit/app.js
@@ -45,12 +45,15 @@ function drawCanvas() {
   if (!img.width || !img.height || !canvas.width || !canvas.height)
     return;  // No img, so just leave canvas cleared.
 
-  var img_wh_aspect = img.width / img.height;
-  var canvas_wh_aspect = canvas.width / canvas.height;
-  var render_wh_aspect = img_wh_aspect / canvas_wh_aspect;
+  var img_wh_ratio = img.width / img.height;
+  var canvas_wh_ratio = canvas.width / canvas.height;
+  var render_wh_ratio = img_wh_ratio / canvas_wh_ratio;
+  var render_hw_ratio = 1 / render_wh_ratio;
+  var render_w = render_wh_ratio > 1 ? 1 : render_wh_ratio;
+  var render_h = render_hw_ratio > 1 ? 1 : render_hw_ratio;
 
   try {
-    cc.drawImage(img, 0, 0, render_wh_aspect, 1);
+    cc.drawImage(img, 0, 0, render_w, render_h);
   } catch (e) {}
 
 }
