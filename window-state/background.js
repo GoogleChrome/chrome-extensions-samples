e20c0821855c79cf840de068564986ca218154f9
Ken Rockot <rockot@chromium.org>
2013-10-01 12:34:41 -0700

    Update window size and screenshot

--- window-state/background.js
+++ window-state/background.js
@@ -1,7 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('window.html', {
     bounds: {
-      width: 600,
+      width: 700,
       height: 600
     }
   });
