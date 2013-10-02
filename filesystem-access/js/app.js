13137ad2a6254b3a25de4da81db977077b181172
Joe Marini <joemarini@hotmail.com>
2013-10-01 21:42:30 -0700

    add directory access to filesystem sample
    Update the fileSystem sample to show how to use the new directory
    access feature in Chrome 31.

--- filesystem-access/js/app.js
+++ filesystem-access/js/app.js
@@ -14,16 +14,33 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 Author: Eric Bidelman (ericbidelman@chromium.org)
+Updated: Joe Marini (joemarini@google.com)
 */
+
+var chosenEntry = null;
+var chooseFileButton = document.querySelector('#choose_file');
+var chooseDirButton = document.querySelector('#choose_dir');
+var saveFileButton = document.querySelector('#save_file');
+var output = document.querySelector('output');
+var textarea = document.querySelector('textarea');
 
 function errorHandler(e) {
   console.error(e);
 }
 
-function displayPath(fileEntry) {
-  chrome.fileSystem.getDisplayPath(fileEntry, function(path) {
-    document.querySelector('#file_path').value = path;
-  });
+function displayEntryData(theEntry) {
+  if (theEntry.isFile) {
+    chrome.fileSystem.getDisplayPath(theEntry, function(path) {
+      document.querySelector('#file_path').value = path;
+    });
+    theEntry.getMetadata(function(data) {
+      document.querySelector('#file_size').textContent = data.size;
+    });    
+  }
+  else {
+    document.querySelector('#file_path').value = theEntry.fullPath;
+    document.querySelector('#file_size').textContent = "N/A";
+  }
 }
 
 function readAsText(fileEntry, callback) {
@@ -58,8 +75,9 @@ function writeFileEntry(writableEntry, opt_blob, callback) {
         writer.seek(0);
         writer.write(opt_blob);
       });
-    } else {
-      chosenFileEntry.file(function(file) {
+    } 
+    else {
+      chosenEntry.file(function(file) {
         writer.truncate(file.fileSize);
         waitForIO(writer, function() {
           writer.seek(0);
@@ -83,43 +101,73 @@ function waitForIO(writer, callback) {
       console.error("Write operation taking too long, aborting!"+
         " (current writer readyState is "+writer.readyState+")");
       writer.abort();
-    } else {
+    } 
+    else {
       callback();
     }
   };
   setTimeout(reentrant, 100);
 }
 
-var chosenFileEntry = null;
-var writeFileButton = document.querySelector('#write_file');
-var chooseFileButton = document.querySelector('#choose_file');
-var saveFileButton = document.querySelector('#save_file');
-var output = document.querySelector('output');
-var textarea = document.querySelector('textarea');
-
-function loadFileEntry(_chosenFileEntry) {
-  chosenFileEntry = _chosenFileEntry;
-  chosenFileEntry.file(function(file) {
-    readAsText(chosenFileEntry, function(result) {
+// for files, read the text content into the textarea
+function loadFileEntry(_chosenEntry) {
+  chosenEntry = _chosenEntry;
+  chosenEntry.file(function(file) {
+    readAsText(chosenEntry, function(result) {
       textarea.value = result;
     });
     // Update display.
-    writeFileButton.disabled = false;
-    saveFileButton.disabled = false;
-    displayPath(chosenFileEntry);
+    saveFileButton.disabled = false; // allow the user to save the content
+    displayEntryData(chosenEntry);
   });
+}
+
+// for directories, read the contents of the top-level directory (ignore sub-dirs)
+// and put the results into the textarea, then disable the Save As button
+function loadDirEntry(_chosenEntry) {
+  chosenEntry = _chosenEntry;
+  if (chosenEntry.isDirectory) {
+    var dirReader = chosenEntry.createReader();
+    var entries = [];
+
+    // Call the reader.readEntries() until no more results are returned.
+    var readEntries = function() {
+       dirReader.readEntries (function(results) {
+        if (!results.length) {
+          textarea.value = entries.join("\n");
+          saveFileButton.disabled = true; // don't allow saving of the list
+          displayEntryData(chosenEntry);
+        } 
+        else {
+          results.forEach(function(item) { 
+            entries = entries.concat(item.fullPath);
+          });
+          readEntries();
+        }
+      }, errorHandler);
+    };
+
+    readEntries(); // Start reading dirs.    
+  }
 }
 
 function loadInitialFile(launchData) {
   if (launchData && launchData.items && launchData.items[0]) {
     loadFileEntry(launchData.items[0].entry);
-  } else {
+  } 
+  else {
+    // see if the app retained access to an earlier file or directory
     chrome.storage.local.get('chosenFile', function(items) {
       if (items.chosenFile) {
-        chrome.fileSystem.restoreEntry(items.chosenFile, function(chosenEntry) {
-          if (chosenEntry) {
-            loadFileEntry(chosenEntry);
-          }
+        // if an entry was retained earlier, see if it can be restored
+        chrome.fileSystem.isRestorable(items.chosenFile, function(bIsRestorable) {
+          // the entry is still there, load the content
+          console.info("Restoring " + items.chosenFile);
+          chrome.fileSystem.restoreEntry(items.chosenFile, function(chosenEntry) {
+            if (chosenEntry) {
+              chosenEntry.isFile ? loadFileEntry(chosenEntry) : loadDirEntry(chosenEntry);
+            }
+          });
         });
       }
     });
@@ -127,35 +175,35 @@ function loadInitialFile(launchData) {
 }
 
 chooseFileButton.addEventListener('click', function(e) {
-  // "type/*" mimetypes aren't respected. Explicitly use extensions for now.
-  // See crbug.com/145112.
   var accepts = [{
-    //mimeTypes: ['text/*'],
+    mimeTypes: ['text/*'],
     extensions: ['js', 'css', 'txt', 'html', 'xml', 'tsv', 'csv', 'rtf']
   }];
-  chrome.fileSystem.chooseEntry({type: 'openFile', accepts: accepts}, function(readOnlyEntry) {
-    if (!readOnlyEntry) {
+  chrome.fileSystem.chooseEntry({type: 'openFile', accepts: accepts}, function(theEntry) {
+    if (!theEntry) {
       output.textContent = 'No file selected.';
       return;
     }
-    chrome.storage.local.set(
-        {'chosenFile': chrome.fileSystem.retainEntry(readOnlyEntry)});
-    loadFileEntry(readOnlyEntry);
+    // use local storage to retain access to this file
+    chrome.storage.local.set({'chosenFile': chrome.fileSystem.retainEntry(theEntry)});
+    loadFileEntry(theEntry);
   });
 });
 
-// writeFileButton.addEventListener('click', function(e) {
-//   if (chosenFileEntry) {
-//    chrome.fileSystem.getWritableEntry(chosenFileEntry, function(writableEntry) {
-//       writeFileEntry(writableEntry, null, function(e) {
-//         output.textContent = 'Write complete :)';
-//       });
-//    });
-//   }
-// });
+chooseDirButton.addEventListener('click', function(e) {
+  chrome.fileSystem.chooseEntry({type: 'openDirectory'}, function(theEntry) {
+    if (!theEntry) {
+      output.textContent = 'No Directory selected.';
+      return;
+    }
+    // use local storage to retain access to this file
+    chrome.storage.local.set({'chosenFile': chrome.fileSystem.retainEntry(theEntry)});
+    loadDirEntry(theEntry);
+  });
+});
 
 saveFileButton.addEventListener('click', function(e) {
-  var config = {type: 'saveFile', suggestedName: chosenFileEntry.name};
+  var config = {type: 'saveFile', suggestedName: chosenEntry.name};
   chrome.fileSystem.chooseEntry(config, function(writableEntry) {
     var blob = new Blob([textarea.value], {type: 'text/plain'});
     writeFileEntry(writableEntry, blob, function(e) {
@@ -166,31 +214,31 @@ saveFileButton.addEventListener('click', function(e) {
 
 // Support dropping a single file onto this app.
 var dnd = new DnDFileController('body', function(data) {
-  chosenFileEntry = null;
+  chosenEntry = null;
   for (var i = 0; i < data.items.length; i++) {
     var item = data.items[i];
     if (item.kind == 'file' &&
         item.type.match('text/*') &&
         item.webkitGetAsEntry()) {
-      chosenFileEntry = item.webkitGetAsEntry();
+      chosenEntry = item.webkitGetAsEntry();
       break;
     }
   };
 
-  if (!chosenFileEntry) {
+  if (!chosenEntry) {
     output.textContent = "Sorry. That's not a text file.";
     return;
-  } else {
-        output.textContent = "";
+  } 
+  else {
+    output.textContent = "";
   }
 
-  readAsText(chosenFileEntry, function(result) {
+  readAsText(chosenEntry, function(result) {
     textarea.value = result;
   });
   // Update display.
-  writeFileButton.disabled = false;
   saveFileButton.disabled = false;
-  displayPath(chosenFileEntry);
+  displayEntryData(chosenEntry);
 });
 
 loadInitialFile(launchData);
