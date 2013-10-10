870a75195f325699b7b0660625c3570655fbbf18
Joe Marini <joemarini@hotmail.com>
2013-10-10 11:25:37 -0700

    Ad windows IDs

--- identity/main.js
+++ identity/main.js
@@ -1,3 +1,3 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', { "bounds": { "width": 1024, "height": 768 } });
+  chrome.app.window.create('index.html', {"id": "IdentityWin", "bounds": { "width": 1024, "height": 768 } });
 });
