882fada6660b4a2e939e5fc26efe4399cad9edb0
Renato Mangini <mangini@chromium.org>
2013-10-15 15:42:04 -0300

    made hello-world responsive; implemented some best practices

--- hello-world/main.js
+++ hello-world/main.js
@@ -12,6 +12,7 @@ chrome.app.runtime.onLaunched.addListener(function() {
   var height = 300;
 
   chrome.app.window.create('index.html', {
+    id: 'main',
     bounds: {
       width: width,
       height: height,
