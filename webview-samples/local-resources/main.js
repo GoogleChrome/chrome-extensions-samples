f4faf64f63a6fe5ec68ef07adbee64ecf760a7c9
Mark Dittmer <mark.s.dittmer@gmail.com>
2014-05-26 13:11:28 -0400

    First stab at local resources sample app

---
+++ webview-samples/local-resources/main.js
@@ -0,0 +1,32 @@
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/apps/app.runtime.html
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+window.chrome.app.runtime.onLaunched.addListener(function() {
+  runApp();
+});
+
+/**
+ * Listens for the app restarting then re-creates the window.
+ *
+ * @see http://developer.chrome.com/apps/app.runtime.html
+ */
+window.chrome.app.runtime.onRestarted.addListener(function() {
+  runApp();
+});
+
+/**
+ * Creates the window for the application.
+ *
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+function runApp() {
+  window.chrome.app.window.create(
+    'good_app.html',
+    {'id': 'GoodWindowID'});
+  window.chrome.app.window.create(
+    'bad_app.html',
+    {'id': 'BadWindowID'});
+}
