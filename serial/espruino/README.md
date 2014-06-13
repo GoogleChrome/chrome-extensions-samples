d7e93348d70da3bc3e72d5b62593338e5962fbd4
Gordon Williams <gw@pur3.co.uk>
2014-06-13 10:21:20 +0100

    First commit of Espruino communication example code

---
+++ serial/espruino/README.md
@@ -0,0 +1,17 @@
+# Espruino LED toggle
+
+This sample lets you toggle the state of an LED on an [Espruino JavaScript board](http://www.espruino.com) as well as reading back its current temperature. The board runs JavaScript, so it's easy to control it by sending JavaScript commands directly to it as strings.
+
+1. Ensure that the line `DEVICE_PATH=` at the top of `main.js` matches Espruino's path
+2. Install and launch this packaged app.
+3. Press either one of the two buttons
+
+
+## APIs
+
+* [Serial API](http://developer.chrome.com/apps/app.hardware.html#serial)
+* [Runtime](http://developer.chrome.com/apps/app.runtime.html)
+* [Window](http://developer.chrome.com/apps/app.window.html)
+     
+## Screenshot
+![screenshot](https://raw.github.com/GoogleChrome/chrome-app-samples/master/serial/espruino/assets/screenshot_1280_800.png)
---
+++ serial/espruino/main.js
@@ -0,0 +1,121 @@
+const DEVICE_PATH = '/dev/ttyACM0';
+const serial = chrome.serial;
+
+/* Interprets an ArrayBuffer as UTF-8 encoded string data. */
+var ab2str = function(buf) {
+  var bufView = new Uint8Array(buf);
+  var encodedString = String.fromCharCode.apply(null, bufView);
+  return decodeURIComponent(escape(encodedString));
+};
+
+/* Converts a string to UTF-8 encoding in a Uint8Array; returns the array buffer. */
+var str2ab = function(str) {
+  var encodedString = unescape(encodeURIComponent(str));
+  var bytes = new Uint8Array(encodedString.length);
+  for (var i = 0; i < encodedString.length; ++i) {
+    bytes[i] = encodedString.charCodeAt(i);
+  }
+  return bytes.buffer;
+};
+
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+
+var SerialConnection = function() {
+  this.connectionId = -1;
+  this.lineBuffer = "";
+  this.boundOnReceive = this.onReceive.bind(this);
+  this.boundOnReceiveError = this.onReceiveError.bind(this);
+  this.onConnect = new chrome.Event();
+  this.onReadLine = new chrome.Event();
+  this.onError = new chrome.Event();
+};
+
+SerialConnection.prototype.onConnectComplete = function(connectionInfo) {
+  if (!connectionInfo) {
+    log("Connection failed.");
+    return;
+  }
+  this.connectionId = connectionInfo.connectionId;
+  chrome.serial.onReceive.addListener(this.boundOnReceive);
+  chrome.serial.onReceiveError.addListener(this.boundOnReceiveError);
+  this.onConnect.dispatch();
+};
+
+SerialConnection.prototype.onReceive = function(receiveInfo) {
+  if (receiveInfo.connectionId !== this.connectionId) {
+    return;
+  }
+
+  this.lineBuffer += ab2str(receiveInfo.data);
+
+  var index;
+  while ((index = this.lineBuffer.indexOf('\n')) >= 0) {
+    var line = this.lineBuffer.substr(0, index + 1);
+    this.onReadLine.dispatch(line);
+    this.lineBuffer = this.lineBuffer.substr(index + 1);
+  }
+};
+
+SerialConnection.prototype.onReceiveError = function(errorInfo) {
+  if (errorInfo.connectionId === this.connectionId) {
+    this.onError.dispatch(errorInfo.error);
+  }
+};
+
+SerialConnection.prototype.connect = function(path) {
+  serial.connect(path, this.onConnectComplete.bind(this))
+};
+
+SerialConnection.prototype.send = function(msg) {
+  if (this.connectionId < 0) {
+    throw 'Invalid connection';
+  }
+  serial.send(this.connectionId, str2ab(msg), function() {});
+};
+
+SerialConnection.prototype.disconnect = function() {
+  if (this.connectionId < 0) {
+    throw 'Invalid connection';
+  }
+  serial.disconnect(this.connectionId, function() {});
+};
+
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+
+var connection = new SerialConnection();
+
+connection.onConnect.addListener(function() {
+  log('connected to: ' + DEVICE_PATH);
+  connection.send('"Hello Espruino"\n');
+});
+
+connection.onReadLine.addListener(function(line) {
+  log('read line: ' + line);
+  // if the line 'TEMPERATURE=' foo is returned, set the
+  // set the button's text
+  if (line.indexOf("TEMPERATURE=")==0)
+    document.querySelector('#get_temperature').innerHTML = "Temp = "+line.substr(12);
+});
+
+connection.connect(DEVICE_PATH);
+
+function log(msg) {
+  var buffer = document.querySelector('#buffer');
+  buffer.innerHTML += msg + '<br/>';
+}
+
+// Toggle LED
+var is_on = false;
+document.querySelector('#toggle').addEventListener('click', function() {
+  is_on = !is_on;
+  connection.send("digitalWrite(LED1, "+(is_on ? '1' : '0')+");\n");
+});
+
+// Get temperature
+document.querySelector('#get_temperature').addEventListener('click', function() {
+  connection.send("console.log('TEMPERATURE='+E.getTemperature().toFixed(1));\n");
+});
+
+
