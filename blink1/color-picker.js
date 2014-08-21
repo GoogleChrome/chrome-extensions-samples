3a0ea550f7e94bee6127aa038dcb4a3ef5da9ad7
Reilly Grant <reillyg@chromium.org>
2014-08-21 10:35:54 -0700

    blink(1) command buffer is only 7 bytes.
    The first byte (as shown in the documentation) is the feature report ID
    and is inserted automatically by the chrome.hid API as necessary.

--- blink1/color-picker.js
+++ blink1/color-picker.js
@@ -64,14 +64,14 @@
     var fade_time = fade_ms / 10;
     var th = (fade_time & 0xff00) >> 8;
     var tl = fade_time & 0x00ff;
-    var data = new Uint8Array(8);
+    var data = new Uint8Array(7);
     data[0] = 'c'.charCodeAt(0);
     data[1] = r;
     data[2] = g;
     data[3] = b;
     data[4] = th;
     data[5] = tl;
-    data[6] = 0;
+    data[6] = led;
     chrome.hid.sendFeatureReport(connection, 1, data.buffer, function() {
       if (chrome.runtime.lastError) {
         console.warn("Unable to set feature report: " +
