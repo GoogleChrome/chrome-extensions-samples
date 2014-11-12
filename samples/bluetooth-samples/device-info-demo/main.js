bad4b2ef43cd70ffa2f53b4d38f32e5d6404595d
Rui Zhao <oahziur@gmail.com>
2014-11-12 10:44:16 -0500

    bluetooth-samples_device-info: Add bluetooth discovery toggle

--- samples/bluetooth-samples/device-info-demo/main.js
+++ samples/bluetooth-samples/device-info-demo/main.js
@@ -16,6 +16,7 @@ var main = (function() {
     // The currently selected service and its characteristics.
     this.service_ = null;
     this.chrcMap_ = {};
+    this.discovering_ = false;
   }
 
   /**
@@ -112,27 +113,36 @@ var main = (function() {
     });
   };
 
+  DeviceInfoDemo.prototype.updateDiscoveryToggleState = function(discovering) {
+    if (this.discovering_ !== discovering) {
+      this.discovering_ = discovering;
+      UI.getInstance().setDiscoveryToggleState(this.discovering_);
+    }
+  };
+
   DeviceInfoDemo.prototype.init = function() {
     // Set up the UI to look like no device was initially selected.
     this.selectService(null);
+
+    // Store the |this| to be used by API callbacks below.
+    var self = this;
 
     // Request information about the local Bluetooth adapter to be displayed in
     // the UI.
     var updateAdapterState = function(adapterState) {
       UI.getInstance().setAdapterState(adapterState.address, adapterState.name);
+      self.updateDiscoveryToggleState(adapterState.discovering);
     };
 
     chrome.bluetooth.getAdapterState(function (adapterState) {
       if (chrome.runtime.lastError)
         console.log(chrome.runtime.lastError.message);
 
+      self.updateDiscoveryToggleState(adapterState.discovering);
       updateAdapterState(adapterState);
     });
 
     chrome.bluetooth.onAdapterStateChanged.addListener(updateAdapterState);
-
-    // Store the |this| to be used by API callbacks below.
-    var self = this;
 
     // Helper functions used below.
     var isKnownDevice = function(deviceAddress) {
@@ -187,6 +197,25 @@ var main = (function() {
                         device.address);
             storeDevice(device.address, device);
           });
+        });
+      }
+    });
+
+    // Set up discovery toggle button handler
+    UI.getInstance().setDiscoveryToggleHandler(function() {
+      if (self.discovering_) {
+        chrome.bluetooth.stopDiscovery(function() {
+          if (chrome.runtime.lastError) {
+            console.log('Failed to stop discovery: ' + chrome.runtime.lastError.message);
+            return;
+          }
+        });
+      } else {
+        chrome.bluetooth.startDiscovery(function() {
+          if (chrome.runtime.lastError) {
+            console.log('Failed to start discovery: ' + chrome.runtime.lastError.message);
+            return;
+          }
         });
       }
     });
