74edb8990e7c55fdf92f18bb3a3e0b25a8c8a7d4
Jake Leichtling <jake.leichtling@gmail.com>
2014-11-13 12:24:22 -0800

    Redoing that

--- samples/in-app-payments-with-server-validation/chromeapp/background.js
+++
@@ -1,8 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('main.html',
-                           {'id': 'ChromeInAppPaymentSample',
-                            'defaultWidth': 900, 'defaultHeight': 500},
-                           function(win) {
-    console.log(win);
-  });
-});
