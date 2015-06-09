1a5b3abb1e9f630619e5a3441f341e03a8c68cd3
Francois Beaufort <beaufort.francois@gmail.com>
2015-06-09 14:11:29 +0200

    [multicast] Updated to the new chrome.sockets.udp API

--- samples/multicast/MulticastSocket.js
+++ samples/multicast/MulticastSocket.js
@@ -9,39 +9,35 @@ function MulticastSocket(config) {
   this.config = config;
 }
 
-function emptyFn() {
-}
+MulticastSocket.prototype.onError = function (message) {};
+MulticastSocket.prototype.onConnected = function () {};
+MulticastSocket.prototype.onDiagram = function (arrayBuffer, remote_address, remote_port) {};
+MulticastSocket.prototype.onDisconnected = function () {};
 
-var mc_proto = MulticastSocket.prototype;
-mc_proto.onError = function (message) {
-};
-mc_proto.onConnected = function () {
-};
-mc_proto.onDiagram = function (arrayBuffer, remote_address, remote_port) {
-};
-mc_proto.onDisconnected = function () {
-};
-
-mc_proto.connect = function (callback) {
+MulticastSocket.prototype.connect = function (callback) {
   var me = this;
-  chrome.socket.create('udp', function (socket) {
-    var socketId = socket.socketId;
-    chrome.socket.setMulticastTimeToLive(socketId, 12, function (result) {
+  chrome.sockets.udp.create({bufferSize: 1024 * 1024}, function (createInfo) {
+    var socketId = createInfo.socketId;
+    var ttl = 12;
+    chrome.sockets.udp.setMulticastTimeToLive(socketId, ttl, function (result) {
       if (result != 0) {
-        me.handleError("Set TTL Error: ", "Unkown error");
+        me.handleError("Set TTL Error: ", "Unknown error");
       }
-      chrome.socket.bind(socketId, "0.0.0.0", me.config.port, function (result) {
+      chrome.sockets.udp.bind(socketId, "0.0.0.0", me.config.port, function (result) {
         if (result != 0) {
-          chrome.socket.destroy(socketId);
-          me.handleError("Error on bind(): ", result);
+          chrome.sockets.udp.close(socketId, function () {
+            me.handleError("Error on bind(): ", result);
+          });
         } else {
-          chrome.socket.joinGroup(socketId, me.config.address, function (result) {
+          chrome.sockets.udp.joinGroup(socketId, me.config.address, function (result) {
             if (result != 0) {
-              chrome.socket.destroy(socketId);
-              me.handleError("Error on joinGroup(): ", result);
+              chrome.sockets.udp.close(socketId, function () {
+                me.handleError("Error on joinGroup(): ", result);
+              });
             } else {
               me.socketId = socketId;
-              me._poll();
+              chrome.sockets.udp.onReceive.addListener(me.onReceive.bind(me));
+              chrome.sockets.udp.onReceiveError.addListener(me.onReceiveError.bind(me));
               me.onConnected();
               if (callback) {
                 callback.call(me);
@@ -54,43 +50,40 @@ mc_proto.connect = function (callback) {
   });
 };
 
-mc_proto.disconnect = function (callback) {
-  var socketId = this.socketId;
-  this.socketId = undefined;
-  chrome.socket.destroy(socketId);
-  this.onDisconnected();
-  if (callback) {
-    callback.call(this);
-  }
+MulticastSocket.prototype.disconnect = function (callback) {
+  var me = this;
+  chrome.sockets.udp.onReceive.removeListener(me.onReceive.bind(me));
+  chrome.sockets.udp.onReceiveError.removeListener(me.onReceiveError.bind(me));
+  chrome.sockets.udp.close(me.socketId, function () {
+    me.socketId = undefined;
+    me.onDisconnected();
+    if (callback) {
+      callback.call(me);
+    }
+  });
 };
 
-mc_proto.handleError = function (additionalMessage, alternativeMessage) {
+MulticastSocket.prototype.handleError = function (additionalMessage, alternativeMessage) {
   var err = chrome.runtime.lastError;
   err = err && err.message || alternativeMessage;
   this.onError(additionalMessage + err);
 };
 
-mc_proto._poll = function () {
-  var me = this;
-  if (me.socketId) {
-    chrome.socket.recvFrom(me.socketId, 1048576, function (result) {
-      if (result.resultCode >= 0) {
-        me.onDiagram(result.data, result.address, result.port);
-        me._poll();
-      } else {
-        me.handleError("", result.resultCode);
-        me.disconnect();
-      }
-    });
-  }
+MulticastSocket.prototype.onReceive = function (info) {
+  this.onDiagram(info.data, info.remoteAddress, info.remotePort);
 };
 
-mc_proto.arrayBufferToString = function (arrayBuffer) {
+MulticastSocket.prototype.onReceiveError = function (socketId, resultCode) {
+  this.handleError("", resultCode);
+  this.disconnect();
+};
+
+MulticastSocket.prototype.arrayBufferToString = function (arrayBuffer) {
   // UTF-16LE
   return String.fromCharCode.apply(String, new Uint16Array(arrayBuffer));
 };
 
-mc_proto.stringToArrayBuffer = function (string) {
+MulticastSocket.prototype.stringToArrayBuffer = function (string) {
   // UTF-16LE
   var buf = new ArrayBuffer(string.length * 2);
   var bufView = new Uint16Array(buf);
@@ -100,38 +93,32 @@ mc_proto.stringToArrayBuffer = function (string) {
   return buf;
 };
 
-mc_proto.sendDiagram = function (message, callback, errCallback) {
+MulticastSocket.prototype.sendDiagram = function (message, callback, errCallback) {
   if (typeof message === 'string') {
     message = this.stringToArrayBuffer(message);
   }
-  if (message && message.byteLength >= 0 && this.socketId) {
-    var me = this;
-    try {
-      chrome.socket.sendTo(this.socketId,
-        message,
-        this.config.address,
-        this.config.port,
-        function (result) {
-          if (result.bytesWritten >= 0) {
-            if (callback) {
-              callback.call(me);
-            }
-          } else {
-            if (errCallback) {
-              errCallback()
-            } else {
-              me.handleError("");
-              if (result.bytesWritten == -15) {
-                me.disconnect();
-              }
-            }
-          }
-        });
-    } catch (e) {
-      me.handleError('Exception: ' + e);
-      me.disconnect();
+  if (!message || message.byteLength == 0 || !this.socketId) {
+    if (callback) {
+      callback.call(this);
     }
-  } else if (callback) {
-    callback.call(this);
+    return;
   }
+  var me = this;
+  chrome.sockets.udp.send(me.socketId, message, me.config.address, me.config.port,
+      function (sendInfo) {
+    if (sendInfo.resultCode >= 0 && sendInfo.bytesSent >= 0) {
+      if (callback) {
+        callback.call(me);
+      }
+    } else {
+      if (errCallback) {
+        errCallback();
+      } else {
+        me.handleError("");
+        if (result.bytesSent == -15) {
+          me.disconnect();
+        }
+      }
+    }
+  });
 };
--- samples/multicast/README.md
+++ samples/multicast/README.md
@@ -5,18 +5,28 @@
 
 ![snapshot](snapshot.png "Snapshot of the app")
 
-This sample shows how to send/recv [multicast socket](http://en.wikipedia.org/wiki/Multicast) diagrams in local network from Chrome apps. It joins the multicast socket group 237.132.123.123 and listens on port 3038.
+This sample shows how to send and receive [multicast
+socket](http://en.wikipedia.org/wiki/Multicast) diagrams in local network from
+Chrome apps. It joins the multicast socket group 237.132.123.123 and listens on
+port 3038.
 
-To support multicast socket messaging in your local network requires the support of you router. The connectivity of this app varies depending on the network configuration.
+To support multicast socket messaging in your local network requires the
+support of your router. The connectivity of this app varies depending on the
+network configuration.
 
-__Warning: This is a simple chatting app demonstrating the usage of multicast socket Chrome app API. It is not designed for reliable communication. Privacy and reachability of the message is NOT guaranteed. It is also possible that a user is able to send to another user while not being able to receive from the latter.__
+__Warning: This is a simple chatting app demonstrating the usage of multicast
+socket Chrome app API. It is not designed for reliable communication. Privacy
+and reachability of the message is NOT guaranteed. It is also possible that a
+user is able to send to another user while not being able to receive from the
+latter.__
+
 ## APIs
 * [Messaging](https://developer.chrome.com/apps/runtime.html)
-* [Runtime](http://developer.chrome.com/apps/app.runtime.html)
-* [Storage](http://developer.chrome.com/apps/storage.html)
-* [Socket](http://developer.chrome.com/apps/socket.html)
-* [Window](http://developer.chrome.com/apps/app.window.html)
-     
+* [Runtime](https://developer.chrome.com/apps/app.runtime.html)
+* [Storage](https://developer.chrome.com/apps/storage.html)
+* [Sockets](https://developer.chrome.com/apps/sockets_udp)
+* [Window](https://developer.chrome.com/apps/app.window.html)
+
 ## Screenshot
 ![screenshot](/samples/multicast/assets/screenshot_1280_800.png)
 
