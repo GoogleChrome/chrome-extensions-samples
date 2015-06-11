505d2a8e01393ae74be726133be1dc7571ba8976
Francois Beaufort <beaufort.francois@gmail.com>
2015-06-11 10:13:30 +0200

    [appview] Fixed nits

--- samples/appview/embedded_app/camera.js
+++ samples/appview/embedded_app/camera.js
@@ -1,5 +1,3 @@
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
-
-navigator.getUserMedia({ video: true }, function(stream) {
+navigator.webkitGetUserMedia({ video: true }, function(stream) {
   document.querySelector('video').src = URL.createObjectURL(stream);
-}, function(error) {});
+}, function(error) { console.log(error) });
