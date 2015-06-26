0889a4d53fe78b93f9beb6312be6a0dbe479ca10
Ehsan Karamad <ekaramad@chromium.org>
2015-06-26 15:03:57 -0400

    Working on findBox.js

--- samples/webview-samples/multi-tab-browser/findBox.js
+++ samples/webview-samples/multi-tab-browser/findBox.js
@@ -1,2 +1,15 @@
 var findTool = (function(container, webview) {
+	function findText(text) {
+	  webview.find(text);
+	}
+	var Finder = function() {
+    this.form = container.querySelector('form');
+    this.searchTextInput = container.querySelector('find-text');
+    this.matchCaseSelection = container.querySelector('match-case');
+    this.searchForward = container.querySelector('find-forward');
+    this.searchBackward = container.querySelector('find-backward');
+  }
+  Finder.prototype.init = function() {
+
+  }
 }(containerElement, webviewElement));
