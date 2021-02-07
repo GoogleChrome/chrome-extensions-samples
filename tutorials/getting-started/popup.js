fe4c8f82e0374539a383602a22402eef1d2be8e4
HannesH_Dev <33062605+HannesGitH@users.noreply.github.com>
2021-02-07 16:56:36 +0100

    fixed minor spelling error in popup.js

--- tutorials/getting-started/popup.js
+++ tutorials/getting-started/popup.js
@@ -1,4 +1,4 @@
-// Initialize butotn with users's prefered color
+// Initialize button with users's prefered color
 let changeColor = document.getElementById("changeColor");
 
 chrome.storage.sync.get("color", ({ color }) => {
