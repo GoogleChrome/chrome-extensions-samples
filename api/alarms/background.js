c94ed65a90ca6c5106673bf5c5ea45831d9f738c
Simeon Vincent <simeonv@google.com>
2021-04-15 21:06:42 -0700

    Open demo on action click

--- api/alarms/background.js
+++ api/alarms/background.js
@@ -4,14 +4,21 @@
 // license that can be found in the LICENSE file or at
 // https://developers.google.com/open-source/licenses/bsd
 
-// Open the extension's demo page on install/update
-chrome.runtime.onInstalled.addListener((_reason) => {
-  chrome.tabs.create({
-    url: chrome.runtime.getURL('index.html')
+// Initialize the demo on install
+chrome.runtime.onInstalled.addListener((reason) => {
+  if (reason !== chrome.runtime.OnInstalledReason.INSTALL) { return }
+
+  openDemoTab();
+
+  // Create an alarm so we have something to look at in the demo
+  chrome.alarms.create('demo-default-alarm', {
+    delayInMinutes: 1,
+    periodInMinutes: 1,
   });
 });
 
-chrome.alarms.create('demo-default-alarm', {
-  delayInMinutes: 1,
-  periodInMinutes: 1,
-});
+chrome.action.onClicked.addListener(openDemoTab);
+
+function openDemoTab() {
+  chrome.tabs.create({ url: 'index.html' });
+}
