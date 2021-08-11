78973da84a598bdecc70afd685c13c8cd59932ab
Simeon Vincent <simeonv@google.com>
2021-08-10 18:13:36 -0700

    Revised to match current doc PR

--- reference/mv3/intro/mv3-migration/content-scripts/popup.js
+++ reference/mv3/intro/mv3-migration/content-scripts/popup.js
@@ -1,24 +1,11 @@
-let injectFunction = document.getElementById('inject-function');
 let injectFile = document.getElementById('inject-file');
+let injectFunction = document.getElementById('inject-function');
 
 async function getCurrentTab() {
   let queryOptions = { active: true, currentWindow: true };
   let [tab] = await chrome.tabs.query(queryOptions);
   return tab;
 }
-
-function showAlert() {
-  alert('Function test alert');
-}
-
-injectFunction.addEventListener('click', async () => {
-  let tab = await getCurrentTab();
-
-  chrome.scripting.executeScript({
-    target: {tabId: tab.id},
-    function: showAlert
-  });
-});
 
 injectFile.addEventListener('click', async () => {
   let tab = await getCurrentTab();
@@ -29,3 +16,17 @@ injectFile.addEventListener('click', async () => {
   });
 });
 
+function showAlert(givenName) {
+  alert(`Hello, ${givenName}`);
+}
+
+injectFunction.addEventListener('click', async () => {
+  let tab = await getCurrentTab();
+
+  let name = 'World';
+  chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    func: showAlert,
+    args: [name]
+  });
+});
