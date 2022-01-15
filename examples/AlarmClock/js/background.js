4c03cc5860839a824a9ed0f248b2c6683a25705a
jxlust <jxljxufe@163.com>
2022-01-15 17:17:15 +0800

    Adds alarm clock example

---
+++ examples/AlarmClock/js/background.js
@@ -0,0 +1,22 @@
+chrome.runtime.onInstalled.addListener(() => {
+  const PERIOD = 1 * .1;
+  const ClockName = "alarm_clock_event";
+
+  chrome.alarms.create(ClockName, {
+    delayInMinutes: 0,
+    periodInMinutes: PERIOD,
+  });
+
+  const options = {
+    type: "basic",
+    title: "AlarmClock",
+    message: `Now the time is:${new Date().getHours()}:${new Date().getMinutes()}`,
+    iconUrl: "../images/logo.png",
+  };
+  chrome.alarms.onAlarm.addListener(function (ev) {
+    if (ev.name == ClockName) {
+      chrome.notifications.create(`MyClock${Date.now()}`, options, function (ev) {
+      });
+    }
+  });
+});
