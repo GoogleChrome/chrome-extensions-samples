03b3fb9cb879a645198ccaa1a08647ab1d8a4a83
Amy Steam <jackandamydev@gmail.com>
2022-04-13 11:03:32 -0500

    Add copyright

--- tutorials/reading-time/scripts/content.js
+++ tutorials/reading-time/scripts/content.js
@@ -1,3 +1,9 @@
+// Copyright 2022 Google LLC
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
 const article = document.querySelector("article");
 
 // `document.querySelector` may return null if the selector doesn't match anything.
