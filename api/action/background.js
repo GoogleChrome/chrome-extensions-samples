d682adad3685c6741fdda79bbb9ec3d11e7f3db8
Simeon Vincent <simeonv@google.com>
2022-04-27 15:17:21 -0700

    Add copyright headers to getting-started

--- api/action/background.js
+++ api/action/background.js
@@ -1,8 +1,16 @@
 // Copyright 2021 Google LLC
 //
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file or at
-// https://developers.google.com/open-source/licenses/bsd
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // Show the demo page once the extension is installed
 chrome.runtime.onInstalled.addListener((_reason) => {
--- api/contextMenus/global_context_search/popup.js
+++ api/contextMenus/global_context_search/popup.js
@@ -1,6 +1,16 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Copyright 2017 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // TLD: top level domain; the "com" in "google.com"
 import { tldLocales } from './locales.js'
@@ -20,14 +30,14 @@ async function createForm() {
     checkbox.addEventListener('click', (event) => {
       handleCheckboxClick(event).catch(console.error)
     })
-  
+
     let span = document.createElement('span');
     span.textContent = locale;
-  
+
     let div = document.createElement('div');
     div.appendChild(checkbox);
     div.appendChild(span);
-  
+
     form.appendChild(div);
   }
 }
@@ -39,10 +49,10 @@ async function handleCheckboxClick(event) {
 
   let { enabledTlds = Object.keys(tldLocales) } = await chrome.storage.sync.get('enabledTlds');
   let tldSet = new Set(enabledTlds)
-  
+
   if (enabled) tldSet.add(tld)
   else tldSet.delete(tld)
-  
+
   await chrome.storage.sync.set({ enabledTlds: [...tldSet] })
 }
 
