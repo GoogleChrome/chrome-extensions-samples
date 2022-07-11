dca19d9ced0ead0310d3508716871a457646b43a
solomonkinard <solomon@kinard.com>
2022-07-11 07:30:48 -0700

    Comments taken into consideration

--- api/favicon/content.js
+++ api/favicon/content.js
@@ -1,6 +1,6 @@
 window.onload = e => {
   const img = document.createElement('img');
-  const url = `chrome-extension://${chrome.runtime.id}/_favicon/?page_url=`;
-  img.src = `${url}https://www.google.com&size=64`;
+  const url = '_favicon/?page_url=https://www.google.com&size=64';
+  img.src = chrome.runtime.getURL(url);
   document.body.appendChild(img);
 }
