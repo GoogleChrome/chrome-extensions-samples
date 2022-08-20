5ac199781bddf470cb600e12956c81b6dc7c0065
Ambushfall <lukathetormentor@gmail.com>
2022-08-20 12:20:28 +0200

    Created MV3 Iframe sandBox Example

---
+++ examples/sandbox/eventpage.js
@@ -0,0 +1,32 @@
+setTimeout(() =>sendMessageToIframe(), 500)// Timeout required to actually wait for the iframe to get generated
+
+
+function sendMessageToIframe() {
+    
+    var iframe = document.getElementById('theFrame');
+    var message = {
+        command: 'render',
+        context: { thing: 'world' }
+    };
+    
+    iframe.contentWindow.postMessage(message, '*');
+    return true
+}
+
+
+window.addEventListener('message', onMessageFromIframeCallback)// attach event listener for iframe's message
+
+
+function onMessageFromIframeCallback(event) {
+    if (event.data.html) {
+        new Notification('Templated!', {
+            icon: 'icon.png',
+            body: 'HTML Received for "' + event.data.name + '": `' +
+                event.data.html + '`'
+        });
+    }
+
+    console.log(event.data.html)
+    document.getElementById('result').innerHTML = event.data.html
+}
+
