323656c97af90406f17d1f5452c073693241ff83
Simeon Vincent <simeonv@google.com>
2023-01-16 09:37:19 -0800

    Missing semi

--- cookbook/offscreen-clipboard-write/background.js
+++ cookbook/offscreen-clipboard-write/background.js
@@ -52,5 +52,5 @@ async function addToClipboard(value) {
 // Solution 2 – Once extension service workers can use the Clipboard API,
 // replace the offscreen document based implementation with something like this.
 async function addToClipboardV2(value) {
-  navigator.clipboard.writeText(value)
+  navigator.clipboard.writeText(value);
 }
