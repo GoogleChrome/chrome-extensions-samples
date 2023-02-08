c3ab47ed2cd892bd4d2bfdc839a4c63dd202c339
Zied Hamdi <zhamdi@gmail.com>
2023-02-08 09:53:52 +0100

    Upgraded code as of manifest v3

--- _archive/mv2/tutorials/broken_background_color/popup.js
+++ _archive/mv2/tutorials/broken_background_color/popup.js
@@ -6,18 +6,26 @@
 
 let changeColor = document.getElementById('changeColor');
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
+chrome.storage.sync.get('color', function (data) {
+    changeColor.style.backgroundColor = data.color;
+    changeColor.setAttribute('value', data.color);
 });
 
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  // The extension must query the active tab
-  // before it can injected a content script
-  chrome.tabs.executeScript(
-      tabs[0].id,
-      // Scripts injected with tabs.executeScript() will not have access to
-      // variables from this context, leaving |color| undefined.
-      {code: 'document.body.style.backgroundColor = color;'});
+changeColor.onclick = function (element) {
+    let color = element.target.value;
+    // The extension must query the active tab
+    // before it can injected a content script
+    // chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        chrome.scripting.executeScript(
+            {
+                target: {tabId: tabs[0].id},
+                function: setColor
+            }
+        );
+    // });
+};
+
+async function setColor() {
+    let {color} = await chrome.storage.sync.get(['color']);
+    document.body.style.backgroundColor = color;
 };
