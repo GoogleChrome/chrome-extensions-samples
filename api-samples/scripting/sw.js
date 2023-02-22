f714bd1ef5762a4dbe4ad595d833d7cac28c6584
Oliver Dunk <oliverdunk@google.com>
2023-02-22 17:16:26 +0000

    Run formatter.

--- api-samples/scripting/sw.js
+++ api-samples/scripting/sw.js
@@ -11,19 +11,21 @@ function openDemoTab() {
 }
 
 chrome.webNavigation.onDOMContentLoaded.addListener(async ({ tabId, url }) => {
-  if (url === "https://example.com/#inject-programmatic") {
-    const { options } = await chrome.storage.local.get("options");
+  if (url === 'https://example.com/#inject-programmatic') {
+    const { options } = await chrome.storage.local.get('options');
     chrome.scripting.executeScript({
       target: { tabId },
-      files: ["content-script.js"],
+      files: ['content-script.js'],
       ...options
     });
   }
 });
 
 chrome.runtime.onMessage.addListener(async ({ name, options }) => {
-  if (name === "inject-programmatic") {
+  if (name === 'inject-programmatic') {
     await chrome.storage.local.set({ options });
-    await chrome.tabs.create({ url: "https://example.com/#inject-programmatic" });
+    await chrome.tabs.create({
+      url: 'https://example.com/#inject-programmatic'
+    });
   }
 });
