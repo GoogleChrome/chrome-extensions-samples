65569d2cd5bd2f84ab0ca6db332f0dd0f65ea75e
Jack Hsieh <chengweih@google.com>
2023-03-27 16:36:57 -0700

    Refactor settings.html and hook up CO2 driver

--- functional-samples/sample.co2meter/background.js
+++ functional-samples/sample.co2meter/background.js
@@ -1,4 +1,5 @@
 import storage from "./modules/storage.js";
+import {getCO2Reading} from "./modules/co2_meter.js";
 
 console.log('Extension service worker background script (background.js)');
 
@@ -17,7 +18,9 @@ chrome.alarms.create("Wakeup to read CO2", {
   periodInMinutes: secondToMinute(storage.getInterval())
 });
 
-chrome.alarms.onAlarm.addListener((alarm) => {
+chrome.alarms.onAlarm.addListener(async (alarm) => {
+  // TODO: to call driver API to check device status and call reading API only if the device is granted and connected.
+  // var co2Reading = await getCO2Reading();
   var co2Reading = 0;
   // TODO: call CO2 driver to read CO2 value.
   storage.saveCO2Value(co2Reading);
--- functional-samples/sample.co2meter/settings_script.js
+++ functional-samples/sample.co2meter/settings_script.js
@@ -1,90 +1,49 @@
 import storage from "./modules/storage.js";
 
-const createGrantDeviceButton = () => {
-  let divElement = document.createElement('div');
-  const buttonElement = document.createElement('button');
-  buttonElement.appendChild(document.createTextNode(`Grant CO2 meter permission`));
-  buttonElement.onclick = () => {
+const setupPermissionButton = () => {
+  // document.querySelector('#grantPermission').addEventListener('click', grantCO2meterPermission);
+  document.querySelector('#grantPermission').onclick = () => {
+    // TODO: call CO2 meter driver to request permission.
     // The extension currently only support this model:
     // https://www.co2meter.com/products/co2mini-co2-indoor-air-quality-monitor
     navigator.hid.requestDevice({ filters: [{ vendorId: 1241, productId: 41042 }] }).then((device) => {
       console.log('CO2 meter permission granted!', device[0]);
     })
   };
-  divElement.appendChild(buttonElement);
-  return divElement;
 }
 
-const createTemperatureMetricButton = () => {
-  let divElement = document.createElement('div');
-  const buttonElement = document.createElement('button');
-  buttonElement.appendChild(document.createTextNode(`Set temperature metric`));
-  buttonElement.onclick = () => {
+const setupTemperatureUnitButton = () => {
+  document.querySelector('#setTemperatureUnit').onclick = () => {
     const metric = document.getElementById("celsius").checked ? 'Celsius' : 'fahrenheit';
     // TODO: to update the storage so that pop up is showing intended metric.
     console.log(`Set temperature metric button clicked, set to use ${metric}`);
     storage.saveTemperatureUnit(metric);
   };
-
-  let celsiusInputElement = document.createElement('input');
-  celsiusInputElement.setAttribute('type', 'radio');
-  celsiusInputElement.setAttribute('id', 'celsius');
-  celsiusInputElement.setAttribute('name', 'celsius');
-  celsiusInputElement.onchange = () => {
+  document.querySelector('#celsius').onchange = () => {
     if (document.getElementById("celsius").checked == true) {
       document.getElementById("fahrenheit").checked = false;
     }
-  }
-  let celsiusLabelElement = document.createElement('label');
-  celsiusLabelElement.setAttribute('for', 'celsius');
-  celsiusLabelElement.innerHTML = 'Celsius';
-
-  let fahrenheitInputElement = document.createElement('input');
-  fahrenheitInputElement.setAttribute('type', 'radio');
-  fahrenheitInputElement.setAttribute('id', 'fahrenheit');
-  fahrenheitInputElement.setAttribute('name', 'fahrenheit');
-  fahrenheitInputElement.onchange = () => {
+  };
+  document.querySelector('#fahrenheit').onchange = () => {
     if (document.getElementById("fahrenheit").checked == true) {
       document.getElementById("celsius").checked = false;
     }
-  }
-  let FahrenheitLabelElement = document.createElement('label');
-  FahrenheitLabelElement.setAttribute('for', 'fahrenheit');
-  FahrenheitLabelElement.innerHTML = 'fahrenheit';
-
-  divElement.appendChild(buttonElement);
-  divElement.appendChild(celsiusInputElement);
-  divElement.appendChild(celsiusLabelElement);
-  divElement.appendChild(fahrenheitInputElement);
-  divElement.appendChild(FahrenheitLabelElement);
-  return divElement;
+  };
 }
 
-const createIntervalButton = () => {
-  let divElement = document.createElement('div');
-  const buttonElement = document.createElement('button');
-  buttonElement.appendChild(document.createTextNode(`Set CO2 reading interval (in seconds)`));
-  buttonElement.onclick = () => {
+const setupIntervalButton = () => {
+  // document.querySelector('#grantPermission').addEventListener('click', grantCO2meterPermission);
+  document.querySelector('#setInterval').onclick = () => {
     const interval = document.getElementById("interval").value;
     // TODO: to update the storage so that pop up is showing intended metric.
     console.log(`Set CO2 reading interval button clicked, set to use ${interval}s`);
     storage.saveInterval(interval);
   };
-
-  let inputElement = document.createElement('input');
-  inputElement.setAttribute('type', 'number');
-  inputElement.setAttribute('id', 'interval');
-  inputElement.setAttribute('name', 'interval');
-  inputElement.setAttribute('value', 10);
-
-  divElement.appendChild(buttonElement);
-  divElement.appendChild(inputElement);
-  return divElement;
 }
 
 window.onload = e => {
   console.log('Settings Page loaded');
-  document.body.appendChild(createGrantDeviceButton());
-  document.body.appendChild(createTemperatureMetricButton());
-  document.body.appendChild(createIntervalButton());
+  setupPermissionButton();
+  setupTemperatureUnitButton();
+  setupIntervalButton();
 };
