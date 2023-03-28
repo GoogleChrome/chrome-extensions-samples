1e41e2e1b552dea64d11cfa4b37940b84160cb4d
Vincent Scheib <scheib@users.noreply.github.com>
2023-03-28 14:25:11 -0700

    Import idb-keyval indexedDB library into storage.js, with trivial usage.

--- functional-samples/sample.co2meter/modules/storage.js
+++ functional-samples/sample.co2meter/modules/storage.js
@@ -1,5 +1,11 @@
+import { get, set, update } from 'https://cdn.jsdelivr.net/npm/idb-keyval@6/+esm';
+
 class Storage {
-    constructor() {}
+    constructor() {
+        // TODO: Move from proof of concept to actual usage of storage.
+        update("storage-test-counter", (val) => (val || 0) + 1);
+        get("storage-test-counter").then(val => console.warn("storage-test-counter", val));
+    }
 
     saveCO2Value(kelvin) {
         console.log("saveCO2Value()", kelvin);
