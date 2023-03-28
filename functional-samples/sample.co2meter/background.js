3baf97cda457fa24273456fc9506fcb4d6267a67
Jack Hsieh <chengweih@google.com>
2023-03-28 13:35:25 -0700

    background page to show CO2 meter disconnected icon

--- functional-samples/sample.co2meter/background.js
+++ functional-samples/sample.co2meter/background.js
@@ -1,27 +1,52 @@
 import storage from "./modules/storage.js";
 import CO2Meter from "./modules/co2_meter.js";
 
-console.log('Extension service worker background script (background.js)');
+const WAKEUP_ALARM = "wakeup alarm";
 
-if (navigator.hid) {
-  console.log('WebHID is available');
-} else {
-  console.log('WebHID not available');
-}
+console.log('Extension service worker background script (background.js)');
 
 const secondToMinute = (seconds) => {
   return seconds / 60;
 }
 
-chrome.alarms.create("Wakeup to read CO2", {
-  delayInMinutes: 0,
-  periodInMinutes: secondToMinute(storage.getInterval())
-});
+const setCO2MeterDisconnectedIcon = () => {
+  chrome.action.setIcon(
+    {path: {'32': 'images/icon32_disconnected.png'}},
+    () => { console.log('Update the extension icon to CO2 disconnected!') },
+  );  
+}
 
-chrome.alarms.onAlarm.addListener(async (alarm) => {
-  // TODO: to call driver API to check device status and call reading API only if the device is granted and connected.
-  // var co2Reading = await getCO2Reading();
-  var co2Reading = 0;
-  // TODO: call CO2 driver to read CO2 value.
-  storage.saveCO2Value(co2Reading);
-});
+const createAlarm = (interval) => {
+  chrome.alarms.create(WAKEUP_ALARM, {
+    delayInMinutes: 0,
+    periodInMinutes: secondToMinute(interval)
+  });
+
+  chrome.alarms.onAlarm.addListener(async () => {
+    if (!CO2Meter.getDeviceStatus()) {
+      chrome.alarms.clear(WAKEUP_ALARM, () => {
+        console.log('Cleared wakeup alarm as CO2 meter is not visible anymore!');
+      })
+      setCO2MeterDisconnectedIcon();
+      return;
+    }
+    var co2Reading = await CO2Meter.getCO2Reading();
+    storage.saveCO2Value(co2Reading);
+  });  
+}
+
+const initilize = async () => {
+  await CO2Meter.init();
+  if (!CO2Meter.getDeviceStatus()) {
+    setCO2MeterDisconnectedIcon();
+    return;
+  }
+  createAlarm(storage.getInterval());
+}
+
+if (navigator.hid) {
+  initilize();
+} else {
+  console.log('WebHID not available!');
+}
+
