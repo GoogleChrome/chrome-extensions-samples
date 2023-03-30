fe39ef3d9e11bd3de78476c10a93f8f708bce3d3
Alpaca Jam <alvinji@google.com>
2023-03-29 19:51:06 -0700

    add  in Storage

--- functional-samples/sample.co2meter/modules/storage.js
+++ functional-samples/sample.co2meter/modules/storage.js
@@ -2,30 +2,29 @@ import { get, set, update } from '../third-party/idb-keyval/dist/index.js';
 
 class Storage {
   constructor() {
-    this.inited = false;
+    this.doneInit = false;
     this.db = null;
     this.getDB = this.getDB.bind(this);
   }
 
-  async init(){
-    this.inited = await get('init');
-    if(!this.inited){
+  async init() {
+    this.doneInit = await get('init');
+    if (!this.doneInit) {
       const request = indexedDB.open('TheDB');
       request.onupgradeneeded = (event) => {
-        console.log('running onupgradeneeded');
+        console.log('indexedDB creates CO2ReadingStore and indexed with time');
         // Create a new object store with an index
         this.db = event.target.result;
-        /*const objectStore = this.db.createObjectStore(OBJECTSTORE, { keyPath: 'id' });*/
-        const objectStore = this.db.createObjectStore('CO2ReadingStore', {autoIncrement: true });
-        objectStore.createIndex('dateIndex', 'date');
+        const objectStore = this.db.createObjectStore('CO2ReadingStore', { autoIncrement: true });
+        objectStore.createIndex('timeIndex', 'time');
 
-        // Add some data to the object store
+        // Some dummy data for testing the object store
         /*
         const data = [
-          { date: 1680127957743, reading: 888 },
-          { date: 1680127958743, reading: 789 },
-          { date: 1680127959743, reading: 989 },
-          { date: 1680127960743, reading: 1089 }
+          { time: 1680127957743, reading: 888 },
+          { time: 1680127958743, reading: 789 },
+          { time: 1680127959743, reading: 989 },
+          { time: 1680127960743, reading: 1089 }
         ];
 
         data.forEach(item => {
@@ -40,32 +39,59 @@ class Storage {
     }
   }
 
-  getDB(e){
+  getDB(e) {
     console.log('getDB store to storage object');
     this.db = e.target.result;
-   }
+  }
 
 
   setCO2Value(ppm) {
     console.log("setCO2Value()", ppm);
 
-    if(!this.db){
+    if (!this.db) {
       console.log("indexedDB is not ready!!");
       return;
     }
 
     const transaction = this.db.transaction('CO2ReadingStore', 'readwrite');
     const objectStore = transaction.objectStore('CO2ReadingStore');
-    const item = {date: new Date().getTime(), reading: ppm};
+    const item = { time: new Date().getTime(), reading: ppm };
     objectStore.add(item);
-    transaction.oncomplete = function(event) {
-      console.log('Transaction completed');
-    };
   };
+
+  getCO2ValueInRange(start, end = new Date().getTime()) {
+    return new Promise(function (resolve, reject) {
+      const self = this;
+      const transaction = self.db.transaction('CO2ReadingStore', 'readonly');
+      const objectStore = transaction.objectStore('CO2ReadingStore');
+
+      // Perform a range query on the index
+      const index = objectStore.index('timeIndex');
+
+      // Query for epoch time between start and end both included and resolve with a array of all reading 
+      const results = [];
+      const range = IDBKeyRange.bound(start, end, false, false);
+      const getRequest = index.openCursor(range);
+      getRequest.onsuccess = function (event) {
+        const cursor = event.target.result;
+        if (cursor) {
+          results.push(cursor.value);
+          cursor.continue();
+        } else {
+          resolve(results);
+        }
+      };
+
+      transaction.onerror = function (event) {
+        reject(event.target.error);
+      };
+    }.bind(this));
+  }
+
 
   setTempValue(kelvin) {
     console.log("setTempValue()", kelvin);
-  };  
+  };
 
   async getIntervalInSeconds() {
     return await get("interval") || 60;
