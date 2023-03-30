abbacfc63e23c9cfdfc7d12f52a7c651c22bd9bd
Vincent Scheib <scheib@users.noreply.github.com>
2023-03-30 13:26:10 -0700

    Continued: chart out of popup into chart.html for iframing

--- functional-samples/sample.co2meter/popup.js
+++ functional-samples/sample.co2meter/popup.js
@@ -1,8 +1,5 @@
 import storage from "./modules/storage.js";
 
 window.onload = async e => {
-  const openTabButtonElement = document.createElement('button');
-  openTabButtonElement.appendChild(document.createTextNode('Settings Page'));
-  openTabButtonElement.onclick = e => { window.open('settings.html', '_blank'); };
-  document.body.appendChild(openTabButtonElement);
+  mainPageButton.onclick = e => { window.open('main-page.html', '_blank'); };
 };
