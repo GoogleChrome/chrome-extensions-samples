1618bf30143875673228321ec7cea34f0b009ebd
Jack Hsieh <chengweih@google.com>
2023-03-30 17:07:14 -0700

    Add calibration period and refactor inputReport

--- functional-samples/sample.co2meter/modules/co2_meter.js
+++ functional-samples/sample.co2meter/modules/co2_meter.js
@@ -21,17 +21,47 @@ import {
 
 const key = new Uint8Array([0xc4, 0xc6, 0xc0, 0x92, 0x40, 0x23, 0xdc, 0x96]);
 
+function parseReport(report) {
+    let data = new Uint8Array(report.data.buffer,
+        report.data.byteOffset,
+        report.data.byteLength);
+
+    const op = data[0];
+    let val = data[1] << 8 | data[2];
+
+    if (op == 0x50) {
+        console.log(`Current CO2 reading is ${val}`);
+        return {[CO2_READING_KEY]: val}
+    } else if (op == 0x42) {
+        val = val / 16;
+        console.log(`Current Temp reading is ${val}`);
+        return {[TEMPERATURE_READING_KEY]: val}
+    } 
+    return {};
+}
+
+function isReadingReady(reading) {
+    return reading && reading.hasOwnProperty(CO2_READING_KEY) && reading.hasOwnProperty(TEMPERATURE_READING_KEY);
+}
+
 class CO2Meter {
     constructor() {
         this.device = null;
-        this.resolver = null;
         this.useVitual = false;
-        this.onInputReport = this.onInputReport.bind(this);
         this.connectClientCB = null;
         this.disconnectClientCB = null;
         this.connectHanlder = this.connectHanlder.bind(this);
         this.disconnectHandler = this.disconnectHandler.bind(this);
         this.reading = null;
+        this.calibration = false;        
+    }
+
+    startCalibration() {
+        this.calibration = true;
+        // Assuming the device takes 20s for calibration.
+        setTimeout(() => {
+            this.calibration = false;
+        }, 20000);    
     }
 
     async init(useVirtual = false) {
@@ -40,6 +70,9 @@ class CO2Meter {
         if (devices.length == 1) this.device = devices[0];
         navigator.hid.addEventListener("connect", this.connectHanlder);
         navigator.hid.addEventListener("disconnect", this.disconnectHandler);
+        // Assuming the worst case that user just plugged in while start
+        // the extension at the same time.
+        this.startCalibration();        
         console.log('CO2Meter init() done');
     }
 
@@ -68,6 +101,7 @@ class CO2Meter {
             && typeof this.connectClientCB === 'function') {
             this.connectClientCB();
         }
+        this.startCalibration();        
     }
 
     disconnectHandler() {
@@ -85,71 +119,53 @@ class CO2Meter {
             return Promise.resolve(999);
         }
 
-        if (!navigator.hid) {
-            console.log(`WebHID is not enabled.`);
-            return;
-        }
-
         if(this.reading) {
             return Promise.reject("Still waiting previous reading promise resolved!");
         }
 
-        this.reading = {};
-
-        await this.device.close();
-
-        try {
-            this.device.addEventListener('inputreport', this.onInputReport);
-            await this.device.open();
-            await this.device.sendFeatureReport(0, key);
-        } catch {
-            this.device.close().then(() => { });
-            this.device.removeEventListener('inputreport', this.onInputReport);
+        if (this.calibration) {
+            return Promise.reject("The CO2 meter is in calibration!");
         }
 
+        this.reading = {};
 
-        return new Promise((resolve, reject) => {
-            this.resolver = resolve;
+        return new Promise(async (resolve, reject) => {
+            const onInputReport = async (report) => {
+                this.reading = Object.assign(this.reading, parseReport(report));
+                if (isReadingReady(this.reading)) {
+                    clearTimeout(timeoutID);
+                    this.device.removeEventListener('inputreport', onInputReport);
+                    await this.device.close();
+                    resolve(this.reading);                    
+                    this.reading = null;   
+                }
+            }
+
             // Currently we have a timeout for 10 sec if CO2 report is not received.
-            setTimeout(() => {
-                reject('Timeout for CO2 reading');
-                this.reading = false;
-            }, 10000);
+            const timeoutID = setTimeout(() => {
+                // this.device might be null if the device is disconnected.
+                if (this.device) {
+                    this.device.removeEventListener('inputreport', onInputReport);
+                }
+                reject('Timeout for CO2 meter reading');
+                this.reading = null;
+            }, 10000);            
+
+            try {
+                this.device.addEventListener('inputreport', onInputReport);
+                await this.device.open();
+                await this.device.sendFeatureReport(0, key);
+            } catch {
+                this.device.removeEventListener('inputreport', onInputReport);                
+                await this.device.close();
+                reject('Fail to open CO2 meter for reading');
+                return;
+            }
         })
     }
 
     getDeviceStatus() {
         return this.useVitual || Boolean(this.device);
-    }
-
-    isReadingReady(reading) {
-        return reading.hasOwnProperty(CO2_READING_KEY) && reading.hasOwnProperty(TEMPERATURE_READING_KEY);
-    }
-
-    onInputReport(report) {
-        const date = new Date();
-
-        let data = new Uint8Array(report.data.buffer,
-            report.data.byteOffset,
-            report.data.byteLength);
-
-        const op = data[0];
-        let val = data[1] << 8 | data[2];
-
-        if (op == 0x50) {
-            console.log(`Current CO2 reading is ${val}`, this.reading);
-            this.reading = Object.assign(this.reading, {[CO2_READING_KEY]: val});
-        } else if (op == 0x42) {
-            val = val / 16;
-            console.log(`Current Temp reading is ${val}`, this.reading);
-            this.reading = Object.assign(this.reading, {[TEMPERATURE_READING_KEY]: val});
-        }
-
-        if (this.isReadingReady(this.reading)) {
-            this.resolver(this.reading);
-            this.device.removeEventListener('inputreport', this.onInputReport);
-            this.device.close();
-        }
     }
 }
 
