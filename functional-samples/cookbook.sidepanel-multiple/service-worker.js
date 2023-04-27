c3ccbb1631fb4d5f02f1ce9a9f58b260f9061bed
amysteam <jackandamydev@gmail.com>
2023-04-27 09:11:11 -0500

    Remove logs

--- functional-samples/cookbook.sidepanel-multiple/service-worker.js
+++ functional-samples/cookbook.sidepanel-multiple/service-worker.js
@@ -1,5 +1,3 @@
-console.log('background');
-
 chrome.runtime.onInstalled.addListener(async () => {
   await chrome.sidePanel.setOptions({
     path: 'sidepanels/welcome-sp.html',
@@ -9,7 +7,6 @@ chrome.runtime.onInstalled.addListener(async () => {
 
 chrome.tabs.onActivated.addListener(async () => {
   const result = await chrome.sidePanel.getOptions({});
-  console.log(result.path);
   if (result.path === 'sidepanels/welcome-sp.html') {
     await chrome.sidePanel.setOptions({
       path: 'sidepanels/main-sp.html',
