8dca89c418f6bbc11205e028adee8805ac3d7ca0
amysteam <jackandamydev@gmail.com>
2023-05-03 10:31:20 -0500

    Add space Change variable

--- functional-samples/cookbook.sidepanel-site-specific/service-worker.js
+++ functional-samples/cookbook.sidepanel-site-specific/service-worker.js
@@ -1,4 +1,5 @@
-const googleURL = 'https://www.google.com';
+const GOOGLE_ORIGIN = 'https://www.google.com';
+
 // Allows users to open the side panel by clicking on the action toolbar icon
 chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
 
@@ -6,7 +7,7 @@ chrome.tabs.onUpdated.addListener((tabId, info, tab) => {
   if (!tab.url) return;
   const url = new URL(tab.url);
   // Enables the side panel on google.com
-  if (url.origin === googleURL) {
+  if (url.origin === GOOGLE_ORIGIN) {
     chrome.sidePanel.setOptions({
       tabId,
       path: 'sidepanel.html',
