cedb3fae1caa28b607d82a389d0e43f403c74db5
amysteam <jackandamydev@gmail.com>
2023-05-04 10:47:18 -0500

    Add error catching and async

--- functional-samples/cookbook.sidepanel-site-specific/service-worker.js
+++ functional-samples/cookbook.sidepanel-site-specific/service-worker.js
@@ -15,21 +15,23 @@
 const GOOGLE_ORIGIN = 'https://www.google.com';
 
 // Allows users to open the side panel by clicking on the action toolbar icon
-chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
 
-chrome.tabs.onUpdated.addListener((tabId, info, tab) => {
+chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
   if (!tab.url) return;
   const url = new URL(tab.url);
   // Enables the side panel on google.com
   if (url.origin === GOOGLE_ORIGIN) {
-    chrome.sidePanel.setOptions({
+    await chrome.sidePanel.setOptions({
       tabId,
       path: 'sidepanel.html',
       enabled: true
     });
   } else {
     // Disables the side panel on all other sites
-    chrome.sidePanel.setOptions({
+    await chrome.sidePanel.setOptions({
       tabId,
       enabled: false
     });
