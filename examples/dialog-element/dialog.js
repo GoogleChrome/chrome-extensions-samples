b41605fe91533c0ba00f4306bdc230de6194a6fb
IanStanion-google <112102430+IanStanion-google@users.noreply.github.com>
2023-05-15 15:23:02 -0400

    (draft) Adding il8n

--- examples/dialog-element/dialog.js
+++ examples/dialog-element/dialog.js
@@ -1,18 +1,19 @@
-window.addEventListener("load", function(e) {
-	var dialog = document.querySelector('#dialog1');
-	document.querySelector('#show').addEventListener("click", function(evt) {
-	  dialog.showModal();
-	});
-	document.querySelector('#close').addEventListener("click", function(evt) {
-	  dialog.close("thanks!");
-	});
+window.addEventListener('load', function (e) {
+  let dialog = document.querySelector('#dialog1');
+  document.querySelector('#show').addEventListener('click', function (evt) {
+    dialog.showModal();
+  });
+  document.querySelector('#close').addEventListener('click', function (evt) {
+    dialog.close('thanks!');
+  });
 
-	dialog.addEventListener("close", function(evt) {
-		document.querySelector('#result').textContent = "You closed the dialog with: " + dialog.returnValue;
-	});
+  dialog.addEventListener('close', function (evt) {
+    document.querySelector('#result').textContent =
+      'You closed the dialog with: ' + dialog.returnValue;
+  });
 
-	// called when the user Cancels the dialog, for example by hitting the ESC key
-	dialog.addEventListener("cancel", function(evt) {
-		dialog.close("canceled");
-	});
+  // called when the user Cancels the dialog, for example by hitting the ESC key
+  dialog.addEventListener('cancel', function (evt) {
+    dialog.close('canceled');
+  });
 });
--- examples/dialog-element/main.js
+++ examples/dialog-element/main.js
@@ -4,9 +4,8 @@
  * @see http://developer.chrome.com/apps/app.runtime.html
  * @see http://developer.chrome.com/apps/app.window.html
  */
-chrome.runtime.onStartup.addListener(function() {
+chrome.runtime.onStartup.addListener(function () {
   chrome.windows.create({
     url: 'index.html'
-    });
-  });   
-
+  });
+});
