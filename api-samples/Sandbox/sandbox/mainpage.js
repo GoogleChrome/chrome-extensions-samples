a8d65318813066bf4427dadccea5d559a5a410fd
IanStanion-google <112102430+IanStanion-google@users.noreply.github.com>
2023-05-19 12:55:12 -0400

    Update api-samples/Sandbox/sandbox/mainpage.js
    Co-authored-by: Joe Medley <jmedley@google.com>

--- api-samples/Sandbox/sandbox/mainpage.js
+++ api-samples/Sandbox/sandbox/mainpage.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var counter=0;
+let counter=0;
 document.addEventListener('DOMContentLoaded', function() {
 
   document.getElementById('reset').addEventListener('click', function(event) {
