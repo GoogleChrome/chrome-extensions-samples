100a37ce0035633fdadf825824ca9cde13c6302c
daidr <daidr@daidr.me>
2023-05-31 15:07:58 +0800

    Add download_links sample

---
+++ api-samples/downloads/download_links/send_links.js
@@ -0,0 +1,35 @@
+{
+  // Send back to the popup a sorted deduped list of valid link URLs on this page.
+  // The popup injects this script into all frames in the active tab.
+
+  let links = [...document.getElementsByTagName('a')];
+  links = links.map(function (element) {
+    // Return an anchor's href attribute, stripping any URL fragment (hash '#').
+    // If the html specifies a relative path, chrome converts it to an absolute
+    // URL.
+    let href = element.href;
+    const hashIndex = href.indexOf('#');
+    if (hashIndex >= 0) {
+      href = href.slice(0, hashIndex);
+    }
+    return href;
+  });
+
+  links.sort();
+
+  // Remove duplicates and invalid URLs.
+  const kBadPrefix = 'javascript';
+  for (let i = 0; i < links.length; ) {
+    if (
+      (i > 0 && links[i] == links[i - 1]) ||
+      links[i] == '' ||
+      kBadPrefix == links[i].toLowerCase().substr(0, kBadPrefix.length)
+    ) {
+      links.splice(i, 1);
+    } else {
+      ++i;
+    }
+  }
+
+  chrome.runtime.sendMessage(links);
+}
