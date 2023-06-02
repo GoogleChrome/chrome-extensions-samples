c27d40cd4309e1f537e4443c2faccaf6f04b8769
Ian Stanion <stanion@google.com>
2023-06-02 09:38:48 -0400

    devtools sample

---
+++ api-samples/devtools/panels/chrome-query/devtools.js
@@ -0,0 +1,28 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// The function below is executed in the context of the inspected page.
+/*global $0*/
+let page_getProperties = function () {
+  let data = window.jQuery && $0 ? jQuery.data($0) : {};
+  // Make a shallow copy with a null prototype, so that sidebar does not
+  // expose prototype.
+  let props = Object.getOwnPropertyNames(data);
+  let copy = { __proto__: null };
+  for (let i = 0; i < props.length; ++i) copy[props[i]] = data[props[i]];
+  return copy;
+};
+
+chrome.devtools.panels.elements.createSidebarPane(
+  'jQuery Properties',
+  function (sidebar) {
+    function updateElementProperties() {
+      sidebar.setExpression('(' + page_getProperties.toString() + ')()');
+    }
+    updateElementProperties();
+    chrome.devtools.panels.elements.onSelectionChanged.addListener(
+      updateElementProperties
+    );
+  }
+);
