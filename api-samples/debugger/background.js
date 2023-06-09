3e5bb6d931c0f808371581b15965439d39b7948c
Ian Stanion <stanion@google.com>
2023-06-09 11:29:54 -0400

    Expands the debugger sample to make use of captured network data

--- api-samples/debugger/background.js
+++ api-samples/debugger/background.js
@@ -1,22 +1,25 @@
-// Copyright 2021 Google LLC
-//
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file or at
-// https://developers.google.com/open-source/licenses/bsd
-
 chrome.action.onClicked.addListener(function (tab) {
   if (tab.url.startsWith('http')) {
-    chrome.debugger.attach({ tabId: tab.id }, '1.0', function () {
+    chrome.debugger.attach({ tabId: tab.id }, '1.2', function () {
       chrome.debugger.sendCommand(
         { tabId: tab.id },
         'Network.enable',
         {},
         function () {
-          console.log('Debugger attached and Network domain enabled');
+          if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError);
+          }
         }
       );
     });
   } else {
     console.log('Debugger can only be attached to HTTP/HTTPS pages.');
+  }
+});
+
+chrome.debugger.onEvent.addListener(function (source, method, params) {
+  if (method === 'Network.responseReceived') {
+    console.log('Response received:', params.response);
+    // Perform your desired action with the response data
   }
 });
