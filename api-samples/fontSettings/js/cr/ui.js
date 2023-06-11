634665091182182260def17c7666a22e240877ae
Ian Stanion <stanion@google.com>
2023-06-11 16:54:56 -0400

    Added mv3 sample for fontSettings

--- api-samples/fontSettings/js/cr/ui.js
+++ api-samples/fontSettings/js/cr/ui.js
@@ -1,7 +1,18 @@
 /* eslint-disable no-undef */
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Ported from MV2 samples. Some coding practices are non-standard in MV3, but the sample remains a robust demonstration of the chrome.fontsettings API.
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 cr.define('cr.ui', function () {
   /**
