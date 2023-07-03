accfdb2dbe88557cff7125fb2faaaab6a628acf6
Oliver Dunk <oliverdunk@google.com>
2023-07-03 10:36:58 +0100

    Add sample for using tabCapture in offscreen document.

---
+++ functional-samples/sample.tabcapture-recorder/README.md
@@ -0,0 +1,18 @@
+# chrome.tabCapture recorder
+
+A sample that demonstrates how to use the [`chrome.tabCapture`](https://developer.chrome.com/docs/extensions/reference/tabCapture/) API to record in the background, using a service worker and [offscreen document].
+
+## Overview
+
+In this sample, clicking the action button starts a recording of the current tab in an offscreen document. After 30 seconds, or once the action button is clicked again, the recording ends and is saved as a download.
+
+## Implementation Notes
+
+See the [Audio recording and screen capture guide](https://developer.chrome.com/docs/extensions/mv3/screen_capture/#audio-and-video) for a more detailed implementation.
+
+## Running this extension
+
+1. Clone this repository.
+2. Load this directory in Chrome as an [unpacked extension](https://developer.chrome.com/docs/extensions/mv3/getstarted/development-basics/#load-unpacked).
+3. Click the extension's action icon to start recording.
+4. Click the extension's action again to stop recording.
---
+++ functional-samples/sample.tabcapture-recorder/offscreen.js
@@ -0,0 +1,79 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+chrome.runtime.onMessage.addListener(async (message) => {
+  if (message.target === 'offscreen') {
+    switch (message.type) {
+      case 'start-recording':
+        startRecording(message.data);
+        break;
+      case 'stop-recording':
+        stopRecording();
+        break;
+      default:
+        throw new Error('Unrecognized message:', message.type);
+    }
+  }
+});
+
+let recorder;
+let data = [];
+
+async function startRecording(streamId) {
+  const media = await navigator.mediaDevices.getUserMedia({
+    audio: {
+      mandatory: {
+        chromeMediaSource: 'tab',
+        chromeMediaSourceId: streamId
+      }
+    },
+    video: {
+      mandatory: {
+        chromeMediaSource: 'tab',
+        chromeMediaSourceId: streamId
+      }
+    }
+  });
+
+  // Continue to play the captured audio to the user.
+  const output = new AudioContext();
+  const source = output.createMediaStreamSource(media);
+  source.connect(output.destination);
+
+  // Start recording.
+  recorder = new MediaRecorder(media, { mimeType: 'video/webm' });
+  recorder.ondataavailable = (event) => data.push(event.data);
+  recorder.onstop = () => {
+    const blob = new Blob(data, { type: 'video/webm' });
+    window.open(URL.createObjectURL(blob), '_blank');
+
+    // Clear state ready for next recording
+    recorder = undefined;
+    data = [];
+  };
+  recorder.start();
+
+  // Record current state in URL
+  window.location.hash = 'recording';
+}
+
+async function stopRecording() {
+  recorder.stop();
+
+  // Stopping the tracks makes sure the recording icon in the tab is removed.
+  recorder.stream.getTracks().forEach((t) => t.stop());
+
+  // Update current state in URL
+  window.location.hash = '';
+}
