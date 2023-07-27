bd56b470d789136a3260124b355e19ef22ae586c
daidr <daidr@daidr.me>
2023-07-27 18:19:02 +0800

    Remove assertion

--- .repo/sample-list-generator/src/constants.ts
+++ .repo/sample-list-generator/src/constants.ts
@@ -1,7 +1,7 @@
 export type FolderTypes = "API_SAMPLE" | "FUNCTIONAL_SAMPLE";
 
 // Define all available folders for samples
-export const AVAILABLE_FOLDERS = [
+export const AVAILABLE_FOLDERS: { path: string, type: FolderTypes }[] = [
   {
     path: 'api-samples',
     type: 'API_SAMPLE'
@@ -10,7 +10,7 @@ export const AVAILABLE_FOLDERS = [
     path: 'functional-samples',
     type: 'FUNCTIONAL_SAMPLE'
   }
-] as { path: string, type: FolderTypes }[];
+];
 
 export const REPO_BASE_URL =
   'https://github.com/GoogleChrome/chrome-extensions-samples/tree/main/';
