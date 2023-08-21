8cb2b9ec0e3a7002bbaa908a7d7523ccc08c78b5
daidr <daidr@daidr.me>
2023-08-21 21:11:51 +0800

    Fix file name

--- api-samples/tabs/inspector/service-worker.js
+++ api-samples/tabs/inspector/service-worker.js
@@ -1,3 +1,5 @@
 chrome.action.onClicked.addListener(function () {
-  chrome.tabs.create({ url: chrome.runtime.getURL('tabs_api.html') });
+  chrome.tabs.create({
+    url: chrome.runtime.getURL('window_and_tabs_manager.html')
+  });
 });
