f30507731227d358b6f56557cecaca8f0bb8debf
Lopez Hugo <hugo.lpz@gmail.com>
2023-12-08 23:24:38 +0100

    Update popup.js
    Just clean up.

--- api-samples/il8n/popup.js
+++ api-samples/il8n/popup.js
@@ -1,3 +1,2 @@
 document.getElementById('title').textContent = chrome.i18n.getMessage('title');
-document.getElementById('content').textContent =
-  chrome.i18n.getMessage('content');
+document.getElementById('content').textContent = chrome.i18n.getMessage('content');
