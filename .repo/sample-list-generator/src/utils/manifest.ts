cdde058f49028f4d9fcb7c0ab62256deb230bd7d
Patrick Kettner <patrickkettner@gmail.com>
2024-01-11 18:04:08 -0500

    support localized file info for extension samples

--- .repo/sample-list-generator/src/utils/manifest.ts
+++ .repo/sample-list-generator/src/utils/manifest.ts
@@ -1,11 +1,32 @@
 import fs from 'fs/promises';
-import { ManifestData } from '../types';
+import { dirname } from 'path';
+import { ManifestData, LocaleData } from '../types';
+const localeRegex = /__MSG_([^_]*)__/
 
 export const getManifest = async (
   manifestPath: string
 ): Promise<ManifestData> => {
   const manifest = await fs.readFile(manifestPath, 'utf8');
   const parsedManifest = JSON.parse(manifest);
+
+  if (manifest.includes('__MSG_')) {
+    const directory = dirname(manifestPath);
+    const localeFile: string = await fs.readFile(`${directory}/_locales/en/messages.json`, 'utf8')
+    const localeData: LocaleData = JSON.parse(localeFile);
+
+    for (const [key, value] of Object.entries(parsedManifest)) {
+      if (typeof value === 'string' && value.startsWith('__MSG_')) {
+        const localeKey: string | undefined = value.match(localeRegex)?.[1];
+
+        if (localeKey) {
+          const localeKeyData = localeData[localeKey]
+          const localeMessage: string = localeKeyData?.message;
+
+          parsedManifest[key] = localeMessage;
+        }
+      }
+    }
+  }
 
   return parsedManifest;
 };
