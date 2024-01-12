bd6bf32b0b4b1b101899937aae785d654efcf36b
Patrick Kettner <patrickkettner@gmail.com>
2024-01-12 13:55:37 -0500

    recursivly search the object rather than searching the stringified version

--- .repo/sample-list-generator/src/utils/manifest.ts
+++ .repo/sample-list-generator/src/utils/manifest.ts
@@ -3,13 +3,26 @@ import { dirname } from 'path';
 import { ManifestData, LocaleData } from '../types';
 const localeRegex = /__MSG_([^_]*)__/
 
+function usesLocaleFiles(obj: object): boolean {
+  // recursively check if any value in a supplied object
+  // is a string that starts with __MSG_. If found, it 
+  // means that the extension uses locale files.
+  return Object.values(obj).some((value) => {
+    if (Object.prototype.toString.call(value) === '[object Object]') {
+      return usesLocaleFiles(value);
+    }
+    return typeof value === 'string' && value.startsWith('__MSG_')
+  });
+}
+
 export const getManifest = async (
   manifestPath: string
 ): Promise<ManifestData> => {
   const manifest = await fs.readFile(manifestPath, 'utf8');
   const parsedManifest = JSON.parse(manifest);
+  const localeKeyUsed = false;
 
-  if (manifest.includes('__MSG_')) {
+  if (usesLocaleFiles(parsedManifest)) {
     const directory = dirname(manifestPath);
     const localeFile: string = await fs.readFile(`${directory}/_locales/en/messages.json`, 'utf8')
     const localeData: LocaleData = JSON.parse(localeFile);
