3c6be9b733a60781e9f6e4d69d4814773b8abd48
Pasquale Riello <pasqualeriello19@hotmail.it>
2024-02-22 14:07:17 +0100

    Update CSS styles
    Change padding, height and font sizes to make the button style more consistent with the navigation bar

--- functional-samples/tutorial.quick-api-reference/content.js
+++ functional-samples/tutorial.quick-api-reference/content.js
@@ -6,8 +6,8 @@
   const { tip } = await chrome.runtime.sendMessage({ greeting: 'tip' });
 
   const tipWidget = createDomElement(`
-    <button type="button" popovertarget="tip-popover" popovertargetaction="show" style="padding: 0;">
-      <span>Tip</span> 
+    <button type="button" popovertarget="tip-popover" popovertargetaction="show" style="padding: 0 12px; height: 36px;">
+      <span style="display: block; font: var(--devsite-link-font,500 14px/20px var(--devsite-primary-font-family));">Tip</span> 
     </button>
   `);
 
