367ac850c6456af22dc307589f85fe18ddad1cdf
Sebastian Benz <sbenz@google.com>
2024-07-22 22:14:50 +0200

    add Gemini API sample

---
+++ functional-samples/ai.gemini-in-the-cloud/README.md
@@ -0,0 +1,15 @@
+# On-device AI with Gemini Nano
+
+This sample demonstrates how to use the Gemini Cloud API in a Chrome Extension.
+
+## Overview
+
+The extension provides a chat interface for the Gemini API. To learn more about the API head over to [https://ai.google.dev/](https://ai.google.dev/).
+
+## Running this extension
+
+1. Clone this repository.
+2. [Retrieve an API key](https://ai.google.dev/gemini-api/docs/api-key) and update [functional-samples/ai.gemini-in-the-cloud/sidepanel/index.js](functional-samples/ai.gemini-in-the-cloud/sidepanel/index.js) (only for testing).
+3. Load this directory in Chrome as an [unpacked extension](https://developer.chrome.com/docs/extensions/mv3/getstarted/development-basics/#load-unpacked).
+4. Click the extension icon.
+5. Interact with the prompt API in the sidebar.
---
+++ functional-samples/ai.gemini-in-the-cloud/sidepanel/index.js
@@ -0,0 +1,135 @@
+import {
+  GoogleGenerativeAI,
+  HarmBlockThreshold,
+  HarmCategory
+} from '../node_modules/@google/generative-ai/dist/index.mjs';
+
+// Important! Do not expose your API in your extension code. You have to
+// options:
+//
+// 1. Let users provide their own API key.
+// 2. Manage API keys in your own server and proxy all calls to the Gemini
+// API through your own server, where you can implement additional security
+// measures such as authentification.
+//
+// It is only OK to put your API key into this file if you're the only
+// user of your extension or for testing.
+const apiKey = ''; // ...;
+
+let genAI = null;
+let model = null;
+let generationConfig = null;
+
+const inputPrompt = document.body.querySelector('#input-prompt');
+const buttonPrompt = document.body.querySelector('#button-prompt');
+const elementResponse = document.body.querySelector('#response');
+const elementLoading = document.body.querySelector('#loading');
+const elementError = document.body.querySelector('#error');
+const sliderTemperature = document.body.querySelector('#temperature');
+const labelTemperature = document.body.querySelector('#label-temperature');
+
+function initModel(generationConfig = {}) {
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_NONE
+    }
+  ];
+  genAI = new GoogleGenerativeAI(apiKey);
+  model = genAI.getGenerativeModel({
+    model: 'gemini-1.5-flash',
+    safetySettings,
+    generationConfig: {
+      // maxOutputTokens: 1000,
+      temperature: generationConfig.temperature
+    }
+  });
+  return model.generationConfig;
+}
+
+async function runPrompt(prompt) {
+  try {
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    console.log(response);
+    return response.text();
+  } catch (e) {
+    console.log('Prompt failed');
+    console.error(e);
+    console.log('Prompt:', prompt);
+    throw e;
+  }
+}
+
+async function initDefaults() {
+  generationConfig = initModel();
+  console.log('Model default:', generationConfig);
+  sliderTemperature.value = generationConfig.temperature;
+  labelTemperature.textContent = generationConfig.temperature;
+  labelTemperature.value = generationConfig.temperature;
+}
+
+initDefaults();
+
+sliderTemperature.addEventListener('input', (event) => {
+  labelTemperature.textContent = event.target.value;
+  generationConfig.temperature = event.target.value;
+});
+
+inputPrompt.addEventListener('input', () => {
+  if (inputPrompt.value.trim()) {
+    buttonPrompt.removeAttribute('disabled');
+  } else {
+    buttonPrompt.setAttribute('disabled', '');
+  }
+});
+
+buttonPrompt.addEventListener('click', async () => {
+  const prompt = inputPrompt.value.trim();
+  showLoading();
+  try {
+    const generationConfig = {
+      temperature: sliderTemperature.value
+    };
+    initModel(generationConfig);
+    const response = await runPrompt(prompt, generationConfig);
+    showResponse(response);
+  } catch (e) {
+    showError(e);
+  }
+});
+
+function showLoading() {
+  hide(elementResponse);
+  hide(elementError);
+  show(elementLoading);
+}
+
+function showResponse(response) {
+  hide(elementLoading);
+  show(elementResponse);
+  // Make sure to preserve line breaks in the response
+  elementResponse.textContent = '';
+  const paragraphs = response.split(/\r?\n/);
+  for (const paragraph of paragraphs) {
+    if (paragraph) {
+      elementResponse.appendChild(document.createTextNode(paragraph));
+    }
+    elementResponse.appendChild(document.createElement('BR'));
+  }
+}
+
+function showError(error) {
+  show(elementError);
+  hide(elementResponse);
+  hide(elementLoading);
+  elementError.textContent = error;
+}
+
+function show(element) {
+  element.removeAttribute('hidden');
+}
+
+function hide(element) {
+  element.setAttribute('hidden', '');
+}
