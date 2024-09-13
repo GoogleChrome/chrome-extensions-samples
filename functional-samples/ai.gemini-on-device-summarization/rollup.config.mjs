97b2813340afe0a9a5bbfaf7e340c98ca7b03e50
Sebastian Benz <sbenz@google.com>
2024-09-13 21:24:56 +0200

    Format markdown output

--- functional-samples/ai.gemini-on-device-summarization/rollup.config.mjs
+++ functional-samples/ai.gemini-on-device-summarization/rollup.config.mjs
@@ -2,22 +2,35 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import copy from 'rollup-plugin-copy';
 
-export default {
-  input: 'scripts/extract-content.js',
-  output: {
-    dir: 'dist/scripts',
-    format: 'cjs'
+export default [
+  {
+    input: 'sidepanel/index.js',
+    output: {
+      dir: 'dist/sidepanel',
+      format: 'es',
+    },
+    plugins: [
+      commonjs(),
+      nodeResolve(),
+      copy({
+        targets: [
+          {
+            src: ['manifest.json', 'background.js', 'sidepanel', 'images'],
+            dest: 'dist'
+          }
+        ]
+      })
+    ]
   },
-  plugins: [
-    commonjs(),
-    nodeResolve(),
-    copy({
-      targets: [
-        {
-          src: ['manifest.json', 'background.js', 'sidepanel', 'images'],
-          dest: 'dist'
-        }
-      ]
-    })
-  ]
-};
+  {
+    input: 'scripts/extract-content.js',
+    output: {
+      dir: 'dist/scripts',
+      format: 'es'
+    },
+    plugins: [
+      commonjs(),
+      nodeResolve(),
+    ]
+  }
+];
