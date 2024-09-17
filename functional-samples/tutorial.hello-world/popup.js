bdc94ff9bc6239b59666310e9aebe62e3663a36c
ghostsofsteel <68242181+ghostsofsteel@users.noreply.github.com>
2024-09-17 19:00:42 -0400

    Add files via upload

--- functional-samples/tutorial.hello-world/popup.js
+++ functional-samples/tutorial.hello-world/popup.js
@@ -1,1 +1,26 @@
-console.log('This is a popup!');
+const textReplacements = [
+    { oldText: 'Watch out! Enemy Rhodesian Ridgeback Pack!', newText: 'THESE DOGS ARE FUCKING CRAZY!' },
+    { oldText: 'Crossroads', newText: 'pee' },
+    { oldText: 'Hoard', newText: 'poo' }
+];
+
+function replaceTextContent() {
+    const walker = document.createTreeWalker(
+        document.body,
+        NodeFilter.SHOW_TEXT,
+        null,
+        false
+    );
+
+    let node;
+    while (node = walker.nextNode()) {
+        let text = node.nodeValue;
+        
+        textReplacements.forEach(({ oldText, newText }) => {
+            text = text.replace(new RegExp(oldText, 'g'), newText);
+        });
+        node.nodeValue = text;
+    }
+}
+
+replaceTextContent();
