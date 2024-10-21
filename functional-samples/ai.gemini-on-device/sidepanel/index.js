e2c1536f6854ab0758c4092c359df24deeb2739d
Gitesh-DF <gitesh@dimension.ai>
2024-10-21 19:33:51 +0530

    Updated: Session creation

--- functional-samples/ai.gemini-on-device/sidepanel/index.js
+++ functional-samples/ai.gemini-on-device/sidepanel/index.js
@@ -12,117 +12,117 @@ const labelTopK = document.body.querySelector('#label-top-k');
 let session;
 
 async function runPrompt(prompt, params) {
-  try {
-    if (!session) {
-      session = await window.ai.assistant.create(params);
+    try {
+        if (!session) {
+            session = await window.ai.languageModel.create(params);
+        }
+        return session.prompt(prompt);
+    } catch (e) {
+        console.log('Prompt failed');
+        console.error(e);
+        console.log('Prompt:', prompt);
+        // Reset session
+        reset();
+        throw e;
     }
-    return session.prompt(prompt);
-  } catch (e) {
-    console.log('Prompt failed');
-    console.error(e);
-    console.log('Prompt:', prompt);
-    // Reset session
-    reset();
-    throw e;
-  }
 }
 
 async function reset() {
-  if (session) {
-    session.destroy();
-  }
-  session = null;
+    if (session) {
+        session.destroy();
+    }
+    session = null;
 }
 
 async function initDefaults() {
-  if (!window.ai) {
-    showResponse('Error: window.ai not supported in this browser');
-    return;
-  }
-  const defaults = await window.ai.assistant.capabilities();
-  console.log('Model default:', defaults);
-  sliderTemperature.value = defaults.temperature;
-  sliderTopK.value = defaults.topK;
-  labelTopK.textContent = defaults.topK;
-  labelTemperature.textContent = defaults.temperature;
-  labelTemperature.value = defaults.temperature;
+    if (!window.ai) {
+        showResponse('Error: window.ai not supported in this browser');
+        return;
+    }
+    const defaults = await window.ai.languageModel.capabilities();
+    console.log('Model default:', defaults);
+    sliderTemperature.value = defaults.temperature;
+    sliderTopK.value = defaults.topK;
+    labelTopK.textContent = defaults.topK;
+    labelTemperature.textContent = defaults.temperature;
+    labelTemperature.value = defaults.temperature;
 }
 
 initDefaults();
 
 buttonReset.addEventListener('click', () => {
-  hide(elementLoading);
-  hide(elementError);
-  hide(elementResponse);
-  reset();
-  buttonReset.setAttribute('disabled', '');
+    hide(elementLoading);
+    hide(elementError);
+    hide(elementResponse);
+    reset();
+    buttonReset.setAttribute('disabled', '');
 });
 
 sliderTemperature.addEventListener('input', (event) => {
-  labelTemperature.textContent = event.target.value;
-  reset();
+    labelTemperature.textContent = event.target.value;
+    reset();
 });
 
 sliderTopK.addEventListener('input', (event) => {
-  labelTopK.textContent = event.target.value;
-  reset();
+    labelTopK.textContent = event.target.value;
+    reset();
 });
 
 inputPrompt.addEventListener('input', () => {
-  if (inputPrompt.value.trim()) {
-    buttonPrompt.removeAttribute('disabled');
-  } else {
-    buttonPrompt.setAttribute('disabled', '');
-  }
+    if (inputPrompt.value.trim()) {
+        buttonPrompt.removeAttribute('disabled');
+    } else {
+        buttonPrompt.setAttribute('disabled', '');
+    }
 });
 
-buttonPrompt.addEventListener('click', async () => {
-  const prompt = inputPrompt.value.trim();
-  showLoading();
-  try {
-    const params = {
-      temperature: sliderTemperature.value,
-      topK: sliderTopK.value
-    };
-    const response = await runPrompt(prompt, params);
-    showResponse(response);
-  } catch (e) {
-    showError(e);
-  }
+buttonPrompt.addEventListener('click', async() => {
+    const prompt = inputPrompt.value.trim();
+    showLoading();
+    try {
+        const params = {
+            temperature: sliderTemperature.value,
+            topK: sliderTopK.value
+        };
+        const response = await runPrompt(prompt, params);
+        showResponse(response);
+    } catch (e) {
+        showError(e);
+    }
 });
 
 function showLoading() {
-  buttonReset.removeAttribute('disabled');
-  hide(elementResponse);
-  hide(elementError);
-  show(elementLoading);
+    buttonReset.removeAttribute('disabled');
+    hide(elementResponse);
+    hide(elementError);
+    show(elementLoading);
 }
 
 function showResponse(response) {
-  hide(elementLoading);
-  show(elementResponse);
-  // Make sure to preserve line breaks in the response
-  elementResponse.textContent = '';
-  const paragraphs = response.split(/\r?\n/);
-  for (const paragraph of paragraphs) {
-    if (paragraph) {
-      elementResponse.appendChild(document.createTextNode(paragraph));
+    hide(elementLoading);
+    show(elementResponse);
+    // Make sure to preserve line breaks in the response
+    elementResponse.textContent = '';
+    const paragraphs = response.split(/\r?\n/);
+    for (const paragraph of paragraphs) {
+        if (paragraph) {
+            elementResponse.appendChild(document.createTextNode(paragraph));
+        }
+        elementResponse.appendChild(document.createElement('BR'));
     }
-    elementResponse.appendChild(document.createElement('BR'));
-  }
 }
 
 function showError(error) {
-  show(elementError);
-  hide(elementResponse);
-  hide(elementLoading);
-  elementError.textContent = error;
+    show(elementError);
+    hide(elementResponse);
+    hide(elementLoading);
+    elementError.textContent = error;
 }
 
 function show(element) {
-  element.removeAttribute('hidden');
+    element.removeAttribute('hidden');
 }
 
 function hide(element) {
-  element.setAttribute('hidden', '');
+    element.setAttribute('hidden', '');
 }
