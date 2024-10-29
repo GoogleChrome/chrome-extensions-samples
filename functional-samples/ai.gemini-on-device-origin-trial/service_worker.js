fa729a8e8905f595274cb4b893c30b4507d15f9f
Thomas Steiner <tomac@google.com>
2024-10-29 14:03:52 +0100

    Rewrite existing example to use the OT

--- functional-samples/ai.gemini-on-device-origin-trial/service_worker.js
+++
@@ -1,7 +0,0 @@
-setTimeout(async () => {
-  const capabilities = await chrome.aiOriginTrial.languageModel.capabilities();
-  console.log(capabilities);
-  const languageModel = await chrome.aiOriginTrial.languageModel.create();
-  console.log(languageModel);
-  console.log(await languageModel.prompt('Tell me a joke'));
-}, 1_000);
--- functional-samples/ai.gemini-on-device/sidepanel/index.js
+++ functional-samples/ai.gemini-on-device/sidepanel/index.js
@@ -14,7 +14,7 @@ let session;
 async function runPrompt(prompt, params) {
   try {
     if (!session) {
-      session = await window.ai.languageModel.create(params);
+      session = await chrome.aiOriginTrial.languageModel.create(params);
     }
     return session.prompt(prompt);
   } catch (e) {
@@ -39,7 +39,7 @@ async function initDefaults() {
     showResponse('Error: window.ai not supported in this browser');
     return;
   }
-  const defaults = await window.ai.languageModel.capabilities();
+  const defaults = await chrome.aiOriginTrial.languageModel.capabilities();
   console.log('Model default:', defaults);
   sliderTemperature.value = defaults.temperature;
   sliderTopK.value = defaults.topK;
