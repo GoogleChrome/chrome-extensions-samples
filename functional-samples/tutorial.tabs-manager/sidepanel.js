fe9c62978bb1acdc76059bf8304bd175be2162f4
Oliver Dunk <oliverdunk@google.com>
2024-11-22 16:26:13 +0000

    Various improvements

--- functional-samples/tutorial.tabs-manager/sidepanel.js
+++ functional-samples/tutorial.tabs-manager/sidepanel.js
@@ -13,26 +13,15 @@
 // limitations under the License.
 
 const tabs = await chrome.tabs.query({
-  url: [
-    'https://developer.chrome.com/docs/webstore/*',
-    'https://developer.chrome.com/docs/extensions/*'
-  ]
+  currentWindow: true
 });
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Collator
-const collator = new Intl.Collator();
-tabs.sort((a, b) => collator.compare(a.title, b.title));
 
 const template = document.getElementById('li_template');
 const elements = new Set();
 for (const tab of tabs) {
   const element = template.content.firstElementChild.cloneNode(true);
 
-  const title = tab.title.split('|')[0].trim();
-  const pathname = new URL(tab.url).pathname.slice('/docs'.length);
-
-  element.querySelector('.title').textContent = title;
-  element.querySelector('.pathname').textContent = pathname;
+  element.querySelector('.title').textContent = tab.title;
   element.querySelector('a').addEventListener('click', async () => {
     // need to focus window as well as the active tab
     await chrome.tabs.update(tab.id, { active: true });
