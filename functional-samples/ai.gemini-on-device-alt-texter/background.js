1667ea3b04c52c8aa45f04671d37529a80fa5de1
Vin Lim <vin.lim.yh@gmail.com>
2025-07-28 22:40:57 +0800

    Revert unrelated commit

--- functional-samples/ai.gemini-on-device-alt-texter/background.js
+++ functional-samples/ai.gemini-on-device-alt-texter/background.js
@@ -19,16 +19,10 @@ async function generateAltText(imgSrc) {
   const imageBitmap = await createImageBitmap(blob);
 
   // Run the prompt
-  const prompt = [{
-    role: 'user',
-    content: [
-      {
-        type: 'text',
-        value: `Please provide a functional, objective description of the provided image in no more than around 30 words so that someone who could not see it would be able to imagine it. If possible, follow an “object-action-context” framework. The object is the main focus. The action describes what’s happening, usually what the object is doing. The context describes the surrounding environment. If there is text found in the image, do your best to transcribe the important bits, even if it extends the word count beyond 30 words. It should not contain quotation marks, as those tend to cause issues when rendered on the web. If there is no text found in the image, then there is no need to mention it. You should not begin the description with any variation of “The image”.`
-      },
-      { type: 'image', value: imageBitmap }
-    ]
-  }];
+  const prompt = [
+    `Please provide a functional, objective description of the provided image in no more than around 30 words so that someone who could not see it would be able to imagine it. If possible, follow an “object-action-context” framework. The object is the main focus. The action describes what’s happening, usually what the object is doing. The context describes the surrounding environment. If there is text found in the image, do your best to transcribe the important bits, even if it extends the word count beyond 30 words. It should not contain quotation marks, as those tend to cause issues when rendered on the web. If there is no text found in the image, then there is no need to mention it. You should not begin the description with any variation of “The image”.`,
+    { type: 'image', content: imageBitmap }
+  ];
   return await session.prompt(prompt);
 }
 
