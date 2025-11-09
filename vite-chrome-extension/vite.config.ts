56cf9ccb190623951af6d56865d684f99534a98c
Niraj Kamdar <nirajkamdar27@pm.me>
2025-11-09 15:47:56 +0400

    feat: vite chrome extension demo

---
+++ vite-chrome-extension/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+    outDir: "dist",
+  },
+});
