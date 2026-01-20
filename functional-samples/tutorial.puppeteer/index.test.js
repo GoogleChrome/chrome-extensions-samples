7fb3b43ad4e02045ed45b1b6ebcdc773838f69a0
Oliver Dunk <oliverdunk@google.com>
2026-01-20 10:09:48 +0000

    Update Puppeteer tutorial to open popup with openPopup()
    Updates the Puppeteer tutorial to use the `action.openPopup()`
    method to open the popup, and then `asPage()` to access the popup
    as a page target.
    
    A basic service worker has been added to the history sample to
    give us a context to call `action.openPopup()` in.

--- functional-samples/tutorial.puppeteer/index.test.js
+++ functional-samples/tutorial.puppeteer/index.test.js
@@ -16,9 +16,9 @@
 const puppeteer = require('puppeteer');
 
 const EXTENSION_PATH = '../../api-samples/history/showHistory';
-const EXTENSION_ID = 'jkomgjfbbjocikdmilgaehbfpllalmia';
 
 let browser;
+let worker;
 
 beforeEach(async () => {
   browser = await puppeteer.launch({
@@ -27,6 +27,15 @@ beforeEach(async () => {
     pipe: true,
     enableExtensions: [EXTENSION_PATH]
   });
+
+  const workerTarget = await browser.waitForTarget(
+    // Assumes that there is only one service worker created by the extension and its URL ends with service-worker.js.
+    (target) =>
+      target.type() === 'service_worker' &&
+      target.url().endsWith('service-worker.js')
+  );
+
+  worker = await workerTarget.worker();
 });
 
 afterEach(async () => {
@@ -35,10 +44,22 @@ afterEach(async () => {
 });
 
 test('one history item is visible', async () => {
+  // Open a page to add a history item.
   const page = await browser.newPage();
-  await page.goto(`chrome-extension://${EXTENSION_ID}/popup.html`);
+  await page.goto('https://example.com');
 
-  const list = await page.$('ul');
+  // Open the extension popup.
+  await worker.evaluate('chrome.action.openPopup();');
+
+  const popupTarget = await browser.waitForTarget(
+    // Assumes that there is only one page with the URL ending with popup.html
+    // and that is the popup created by the extension.
+    (target) => target.type() === 'page' && target.url().endsWith('popup.html')
+  );
+
+  const popup = await popupTarget.asPage();
+
+  const list = await popup.$('ul');
   const children = await list.$$('li');
 
   expect(children.length).toBe(1);
